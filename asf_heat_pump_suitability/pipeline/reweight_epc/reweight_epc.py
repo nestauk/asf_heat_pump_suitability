6355f95062c5b8001ac57e3f072f08d636b5b9b0
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-05-16 13:40:39 +0100

    add functions to reweight_epc.py to prepare feature columns in EPC sample

---
+++ asf_heat_pump_suitability/pipeline/reweight_epc/reweight_epc.py
@@ -0,0 +1,88 @@
+import polars as pl
+from asf_heat_pump_suitability import config
+
+
+def add_col_property_type(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Add `property_type` column to EPC dataset with property type categories corresponding to those from the census.
+
+    Args:
+        df (pl.DataFrame): EPC dataset
+
+    Returns:
+        pl.DataFrame: EPC dataset with `property_type` column
+    """
+    terraced = [
+        "Mid-Terrace",
+        "End-Terrace",
+        "Enclosed Mid-Terrace",
+        "Enclosed End-Terrace",
+    ]
+
+    df = df.with_columns(
+        pl.when(
+            pl.col("PROPERTY_TYPE").is_in(["House", "Bungalow"]),
+            pl.col("BUILT_FORM") == "Detached",
+        )
+        .then(pl.lit("Detached whole house or bungalow"))
+        .when(
+            pl.col("PROPERTY_TYPE").is_in(["House", "Bungalow"]),
+            pl.col("BUILT_FORM") == "Semi-Detached",
+        )
+        .then(pl.lit("Semi-detached whole house or bungalow"))
+        .when(
+            pl.col("PROPERTY_TYPE").is_in(["House", "Bungalow"]),
+            pl.col("BUILT_FORM").is_in(terraced),
+        )
+        .then(pl.lit("Terraced (including end-terrace) whole house or bungalow"))
+        .when(pl.col("PROPERTY_TYPE").is_in(["Flat", "Maisonette"]))
+        .then(pl.lit("Flat, maisonette or apartment"))
+        .when(pl.col("PROPERTY_TYPE").is_in(["Park Home"]))
+        .then(pl.lit("A caravan or other mobile or temporary structure"))
+        .alias("property_type")
+    )
+
+    return df
+
+
+def add_col_nrooms(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Add `number_of_rooms` column to EPC dataset with categories corresponding to those from the census.
+
+    Args:
+        df (pl.DataFrame): EPC dataset
+
+    Returns:
+        pl.DataFrame: EPC dataset with `number_of_rooms` column
+    """
+    return df.with_columns(
+        pl.col("NUMBER_HABITABLE_ROOMS")
+        .map_elements(lambda x: 9 if x > 9 else x, return_dtype=pl.Float32)
+        .cast(pl.Int16)
+        .alias("number_of_rooms")
+    )
+
+
+def add_col_build_year_1930(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Add `build_year` column to EPC dataset indicating whether property construction year is pre- or post-1930 (or unknown).
+
+    Args:
+        df (pl.DataFrame): EPC dataset
+
+    Returns:
+        pl.DataFrame: EPC dataset with `build_year` column
+    """
+    pre = config["mapping"]["build_year_pre_cols_epc"]
+    post = config["mapping"]["build_year_post_cols_epc"]
+
+    df = df.with_columns(
+        pl.when(pl.col("CONSTRUCTION_AGE_BAND").is_in(pre))
+        .then(pl.lit("pre_1930"))
+        .when(pl.col("CONSTRUCTION_AGE_BAND").is_in(post))
+        .then(pl.lit("post_1930"))
+        .otherwise(pl.lit("unknown"))
+        .alias("build_year")
+    )
+
+    return df
