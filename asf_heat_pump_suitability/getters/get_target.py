83f9f886a6978cc0086c7b7947a4ed8770dabd70
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-05-21 10:07:24 +0100

    make changes requested in review - clean return statements of functions - raise warning if fill_censored value is outside suggested range - update docstrings of getters/get_target.py functions for clarity

--- asf_heat_pump_suitability/getters/get_target.py
+++ asf_heat_pump_suitability/getters/get_target.py
@@ -1,4 +1,5 @@
 import polars as pl
+import warnings
 
 from asf_heat_pump_suitability import config
 from asf_heat_pump_suitability.getters import base_getters
@@ -6,36 +7,40 @@ from asf_heat_pump_suitability.getters import base_getters
 
 def get_df_target_nrooms() -> pl.DataFrame:
     """
-    Get dataframe of counts of total number of rooms for properties in all LSOAs in England and Wales. Source: census data 2021.
+    Get dataframe of counts of total number of rooms for properties in all LSOAs in England and Wales. Where number of rooms
+    is >= 9, counts are combined into `9+` category. Source: census data 2021.
 
     Returns:
         pl.Dataframe: counts of total number of rooms for properties in all LSOAs in England and Wales
     """
     df = pl.read_csv(config["data_source"]["EW_census_number_of_rooms"])
-    df = df.drop(
-        [
-            "Lower layer Super Output Areas",
-            "Number of rooms (Valuation Office Agency) (9 categories)",
-        ]
+    df = (
+        df.drop(
+            [
+                "Lower layer Super Output Areas",
+                "Number of rooms (Valuation Office Agency) (9 categories)",
+            ]
+        )
+        .rename(
+            {
+                "Lower layer Super Output Areas Code": "lsoa",
+                "Number of rooms (Valuation Office Agency) (9 categories) Code": "number_of_rooms",
+            }
+        )
+        .pivot(index="lsoa", columns="number_of_rooms", values="Observation")
+        .rename({"9": "9+"})
     )
-    df = df.rename(
-        {
-            "Lower layer Super Output Areas Code": "lsoa",
-            "Number of rooms (Valuation Office Agency) (9 categories) Code": "number_of_rooms",
-        }
-    )
-    return df.pivot(
-        index="lsoa", columns="number_of_rooms", values="Observation"
-    ).rename({"9": "9+"})
+
+    return df
 
 
-def get_df_target_property_type(censored_values: int = 0) -> pl.DataFrame:
+def get_df_target_property_type(fill_censored: int = 0) -> pl.DataFrame:
     """
     Get dataframe of property type counts for all LSOAs in England and Wales, and fill censored values with given
     constant. Source: census data 2021.
 
     Args:
-        censored_values (int): value to fill censored values with, 0-9. Default 0.
+        fill_censored (int): value to fill censored values with, [0-10]. Default 0.
 
     Returns:
         pl.Dataframe: counts of property type for all LSOAs in England and Wales
@@ -46,21 +51,26 @@ def get_df_target_property_type(censored_values: int = 0) -> pl.DataFrame:
     df = pl.read_excel(content, sheet_name="2c", engine="calamine")
 
     # Remove empty header rows
-    df = df.rename(df[2].to_dicts().pop()).slice(
-        3,
+    df = (
+        df.rename(df[2].to_dicts().pop())
+        .slice(
+            3,
+        )
+        .drop(["Area Name"])
+        .rename({"Area Code": "lsoa"})
     )
-    df = df.rename({"Area Code": "lsoa"}).drop(["Area Name"])
+    df = _fill_df_censored_values(df, fill_censored)
 
-    return _fill_df_censored_values(df, censored_values)
+    return df
 
 
-def get_df_target_tenure(censored_values: int = 0) -> pl.DataFrame:
+def get_df_target_tenure(fill_censored: int = 0) -> pl.DataFrame:
     """
     Get dataframe of tenure type counts for all LSOAs in England and Wales, and fill censored values with given
     constant. Source: census data 2021.
 
     Args:
-        censored_values (int): value to fill censored values with, 0-9. Default 0.
+        fill_censored (int): value to fill censored values with, [0-10]. Default 0.
 
     Returns:
         pl.Dataframe: counts of tenure type for all LSOAs in England and Wales
@@ -71,24 +81,31 @@ def get_df_target_tenure(censored_values: int = 0) -> pl.DataFrame:
     df = pl.read_excel(content, sheet_name="3c", engine="calamine")
 
     # Remove empty header rows
-    df = df.rename(df[2].to_dicts().pop()).slice(
-        3,
+    df = (
+        df.rename(df[2].to_dicts().pop())
+        .slice(
+            3,
+        )
+        .drop(["Area Name"])
+        .rename(
+            {
+                "Area Code": "lsoa",
+                "Owned or shared ownership": "owner-occupied",
+                "Social Rented": "rental (social)",
+                "Private Rented or lives rent free": "rental (private)",
+            }
+        )
     )
-    df = df.rename(
-        {
-            "Area Code": "lsoa",
-            "Owned or shared ownership": "owner-occupied",
-            "Social Rented": "rental (social)",
-            "Private Rented or lives rent free": "rental (private)",
-        }
-    ).drop(["Area Name"])
 
-    return _fill_df_censored_values(df, censored_values)
+    df = _fill_df_censored_values(df, fill_censored)
+
+    return df
 
 
 def get_df_target_tenure_uncensored() -> pl.DataFrame:
     """
-    Get dataframe of tenure type counts for all LSOAs in England and Wales. Source: census data 2021.
+    Get dataframe of tenure type counts for all LSOAs in England and Wales. Dataframe has no censored values. Source:
+    census data 2021.
 
     Returns:
         pl.Dataframe: counts of tenure type for all LSOAs in England and Wales
@@ -136,18 +153,18 @@ def get_df_target_tenure_uncensored() -> pl.DataFrame:
 
 
 def get_df_target_build_year(
-    year_label: int = 1930,
     pre_cols: list = config["mapping"]["build_year_pre_cols"],
     post_cols: list = config["mapping"]["build_year_post_cols"],
+    year_label: int = 1930,
 ) -> pl.DataFrame:
     """
-    Get dataframe of counts of properties built before and after given year for all LSOAs in England and Wales. Source:
-    Consumer Data Research Centre, 2015.
+    Get dataframe of counts of properties built before and after given year (determined by `pre_cols` and `post_cols`
+    params) for all LSOAs in England and Wales. Source: Consumer Data Research Centre, 2015.
 
     Args:
+        pre_cols (list): column names of age bands before age split. Default pre-1930 columns.
+        post_cols (list): column names of age bands after age split. Default post-1930 columns.
         year_label (int): build year when split occurs
-        pre_cols (list): columns before age split. Default pre-1930 columns.
-        post_cols (list): columns after age split. Default post-1930 columns.
 
     Returns:
         pl.Dataframe: counts of properties built before and after given year for all LSOAs in England and Wales.
@@ -173,11 +190,18 @@ def _fill_df_censored_values(df: pl.DataFrame, val: int) -> pl.DataFrame:
 
     Args:
         df (pl.DataFrame): dataframe
-        val (int): value to fill censored values with
+        val (int): value to fill censored values with, [0-10]
 
     Returns:
         pl.DataFrame: dataframe with filled values
     """
+    if not (0 <= val <= 10):
+        warnings.warn(
+            "Value to fill censored target data should be within range [0-10]. "
+            "Values outside this range may significantly change target proportions."
+        )
     cols = df.columns
     cols.remove("lsoa")
-    return df.with_columns([pl.col(cols).str.replace("c", f"{val}").cast(pl.Int64)])
+    df = df.with_columns([pl.col(cols).str.replace("c", f"{val}").cast(pl.Int64)])
+
+    return df
