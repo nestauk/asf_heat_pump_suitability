ad1bc8e780b59fb314ce0d95a29ca6913bab5dba
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-05-24 18:17:09 +0100

    add function to round proportions to sum to 1 in prepare_target.py

--- asf_heat_pump_suitability/pipeline/reweight_epc/prepare_target.py
+++ asf_heat_pump_suitability/pipeline/reweight_epc/prepare_target.py
@@ -1,3 +1,4 @@
+import random
 import balance
 from balance.weighting_methods import rake
 import polars as pl
@@ -14,19 +15,37 @@ def generate_balance_target_population(
 
     Args:
         target_marginals (Dict[str, dict]): dict of dicts containing target proportions for each feature category by LSOA
-        lsoa (str): LSOA to generate target population for
+        lsoa (str): LSOA code to generate target population for
 
     Returns:
-        balance.sample_class.Sample: artificial target population object generated from given marginals
+        balance.sample_class.Sample: artificial target population object generated from given marginals for single LSOA
     """
-    target = {k: v[lsoa] for k, v in target_marginals.items()}
+    target = get_dict_lsoa_marginals(target_marginals=target_marginals, lsoa=lsoa)
+    target = _get_dict_proportions_sum_one(lsoa_marginals=target, round_n=3)
     df = rake.prepare_marginal_dist_for_raking(target)
     df["weight"] = 1
     df = balance.Sample.from_frame(df, id_column="id", weight_column="weight")
     return df
 
 
-def get_dict_target_marginals() -> Dict[str, pl.DataFrame]:
+def get_dict_lsoa_marginals(target_marginals, lsoa):
+    """
+    Get dict of dicts containing target proportions of each feature category for single specified LSOA. Dictionary keys
+    are feature names.
+
+    Args:
+        target_marginals (Dict[str, dict]): dict of dicts containing target proportions for each feature category per LSOA
+        lsoa (str): LSOA code to get target marginals for
+
+    Returns:
+        Dict[str, dict]: dict of dicts containing target proportions for each feature category for single specified
+        LSOA
+    """
+    lsoa_marginals = {k: v[lsoa] for k, v in target_marginals.items()}
+    return lsoa_marginals
+
+
+def get_dict_target_marginals() -> Dict[str, dict]:
     """
     Get dict of dicts containing target proportions of each feature category per LSOA. Dictionary keys are
     feature names.
@@ -39,8 +58,9 @@ def get_dict_target_marginals() -> Dict[str, pl.DataFrame]:
     target_proportions = {
         k: convert_df_proportions(v) for k, v in target_features.items()
     }
+    marginals = {k: to_dict_feature_marginals(v) for k, v in target_proportions.items()}
 
-    return {k: to_dict_feature_marginals(v) for k, v in target_proportions.items()}
+    return marginals
 
 
 def get_dict_dfs_counts() -> Dict[str, pl.DataFrame]:
@@ -52,13 +72,15 @@ def get_dict_dfs_counts() -> Dict[str, pl.DataFrame]:
         Dict[str, pl.DataFrame]: dict of dataframes containing counts of each feature category per LSOA in the target
         datasets
     """
-    return {
-        "tenure": get_target.get_df_target_tenure(),
-        "property_type": get_target.get_df_target_property_type(),
+    count_dict = {
+        "tenure": get_target.get_df_target_tenure_uncensored(),
+        "property_type": get_target.get_df_target_property_type_uncensored(),
         # TODO: potentially collapse nrooms categories to increase speed
-        "nrooms": get_target.get_df_target_nrooms(),
+        # "nrooms": get_target.get_df_target_nrooms(),
         "build_year": get_target.get_df_target_build_year(),
     }
+
+    return count_dict
 
 
 def convert_df_proportions(df: pl.DataFrame) -> Dict[str, pl.DataFrame]:
@@ -81,6 +103,34 @@ def convert_df_proportions(df: pl.DataFrame) -> Dict[str, pl.DataFrame]:
     return df
 
 
+def _get_dict_proportions_sum_one(
+    lsoa_marginals: Dict[str, dict], round_n: int = 3
+) -> Dict[str, dict]:
+    """
+    Ensure target proportions sum to 1 for each feature.
+
+    Args:
+        lsoa_marginals (Dict[str, dict]): dict of dicts containing target proportions for each feature category for single specified
+        LSOA
+        round_n (int): decimal places to round remainder to
+
+    Returns:
+        Dict[str, dict]: dict of dicts containing target proportions for each feature category for single specified
+        LSOA, where proportions add to 1.
+    """
+    # Make sure rounded numbers add to 1
+    for k, v in lsoa_marginals.items():
+        diff = round(
+            1 - sum(v.values()), round_n
+        )  # Otherwise this can be e.g. 0.0010000001
+        if diff != 0:
+            lsoa_marginals[k][
+                random.choice(list(v.keys()))
+            ] += diff  # Choosing a random key to change so that we don't over bias one category more than others. Note - this means the results will be stochastic.
+    print({k: sum(v.values()) for k, v in lsoa_marginals.items()})
+    return lsoa_marginals
+
+
 def to_dict_feature_marginals(df: pl.DataFrame) -> Dict[str, dict]:
     """
     Convert dataframe with target marginals to dict where keys are LSOA codes
@@ -93,5 +143,6 @@ def to_dict_feature_marginals(df: pl.DataFrame) -> Dict[str, dict]:
     """
     lsoas = df["lsoa"].to_list()
     marginals = df.select(pl.exclude("lsoa")).to_dicts()
+    marginals = dict(zip(lsoas, marginals))
 
-    return dict(zip(lsoas, marginals))
+    return marginals
