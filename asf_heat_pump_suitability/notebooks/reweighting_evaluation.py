5979d2666304880851652cb4b052c75e79887207
lizgzil <lizgzil@hotmail.com>
2024-06-12 19:20:19 +0100

    Add the analysis of the evaluation notebook

---
+++ asf_heat_pump_suitability/notebooks/reweighting_evaluation.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     comment_magics: true
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: ''
+#     name: ''
+# ---
+
+# %%
