13f52c7a735d962127117da2f65821f0cbf32901
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-06-24 18:45:49 +0100

    add functions to land_extent.py - add functions to get bounding polygon/bbox of INSPIRE files - add functions to transform INSPIRE gdfs

--- asf_heat_pump_suitability/pipeline/prepare_features/land_extent.py
+++ asf_heat_pump_suitability/pipeline/prepare_features/land_extent.py
@@ -2,30 +2,64 @@ import pandas as pd
 import geopandas as gpd
 import shapely
 import regex as re
+from asf_heat_pump_suitability import config
 from asf_heat_pump_suitability.getters import base_getters, get_datasets
+from asf_heat_pump_suitability.utils import geo_utils
+
+
+def transform_gdf_council_bounds(
+    ladnm_col: str,
+    keep_cols: list,
+) -> gpd.GeoDataFrame:
+    """
+    Transform council (local authority district) geo dataset: standardise council names (lower and remove special
+    characters and spaces) and convert geometries to British National Grid CRS.
+
+    Args:
+        ladnm_col (str): name of column with council (LAD) names
+        keep_cols (list): names of columns to keep
+
+    Returns:
+        gpd.GeoDataFrame: dataframe of council (LAD) names with geometries in British National Grid CRS
+    """
+    council_bounds = get_datasets.load_gdf_ons_council_bounds()
+    council_bounds = council_bounds[keep_cols]
+    council_bounds["council_name_std"] = _standardise_list_council_names(
+        council_bounds[ladnm_col]
+    )
+    council_bounds = council_bounds.to_crs(epsg="27700")
+
+    return council_bounds
 
 
 def generate_gdf_map_file_to_bounds(
-    land_extent_location,
+    land_extent_location: str = config["data_source"]["EW_inspire_land_extent"],
     ladnm_col: str = "LAD23NM",
     keep_cols: list = ["LAD23NM", "LAD23CD", "geometry"],
-):
-    """ """
-    council_bounds = get_datasets.load_gdf_ons_council_bounds()
-    council_names = _standardise_list_council_names(council_bounds[ladnm_col])
-    council_bounds = council_bounds[keep_cols]
-    council_bounds["council_name_std"] = council_names
+) -> gpd.GeoDataFrame:
+    """
+    Create dataframe with land extent filenames and their matching bounding polygons by matching land extent filenames
+    to ONS council polygon names.
 
+    Args:
+        land_extent_location (str): location of land extent files. Default S3 location.
+        ladnm_col (str): name of column with council (LAD) names in council polygons file
+        keep_cols (list): names of columns to keep in council polygons file
+
+    Returns:
+        gpd.GeoDataFrame: dataframe with land extent filenames and their matching bounding polygons
+    """
+    council_bounds = transform_gdf_council_bounds(ladnm_col, keep_cols)
     land_extent_files = base_getters.list_files_s3_location(land_extent_location)
+
     matches = _match_list_file_to_name(
-        land_extent_files=land_extent_files, council_names=council_names
+        land_extent_files=land_extent_files,
+        council_names=council_bounds["council_name_std"],
     )
 
     matches = pd.DataFrame(
-        {"file_name": land_extent_files, "council_bounds_matches": matches}
-    )
-
-    matches = matches.explode("council_bounds_matches")
+        {"inspire_file_name": land_extent_files, "council_bounds_matches": matches}
+    ).explode("council_bounds_matches")
 
     file_to_bounds = matches.merge(
         council_bounds,
@@ -33,6 +67,11 @@ def generate_gdf_map_file_to_bounds(
         left_on="council_bounds_matches",
         right_on="council_name_std",
     )
+
+    file_to_bounds = gpd.GeoDataFrame(
+        file_to_bounds, crs="EPSG:27700", geometry="geometry"
+    )
+    file_to_bounds = fill_nulls_file_bounds(file_to_bounds)
 
     return file_to_bounds
 
@@ -57,33 +96,48 @@ def _match_list_file_to_name(land_extent_files, council_names):
     return matches
 
 
-def extend_gdf_parcel_bboxes(path: str) -> gpd.GeoDataFrame:
+def fill_nulls_file_bounds(gdf):
     """ """
-    gdf = base_getters.load_gdf_from_s3_geojson(s3_uri=path)
-    gdf["geom_types"] = gdf["geometry"].geom_type
-    gdf["bbox"] = gdf["geometry"].apply(lambda x: x.bounds)
-    gdf[["minx", "miny", "maxx", "maxy"]] = gpd.GeoDataFrame(
-        gdf["bbox"].tolist(), index=gdf.index
-    )
+    missing_bbox = gdf[gdf["council_bounds_matches"].isnull()][
+        "inspire_file_name"
+    ].to_list()
+    for file in missing_bbox:
+        file_polygon = get_polygon_file_bounds(f"s3://{file}")
+        gdf.loc[gdf["inspire_file_name"] == file, "geometry"] = file_polygon
 
     return gdf
 
 
-def create_dict_file_bounds(gdf) -> dict:
+def get_polygon_file_bounds(path: str) -> shapely.Polygon:
     """ """
-    bounds = {
-        "minx": gdf["minx"].min(),
-        "miny": gdf["miny"].min(),
-        "maxx": gdf["maxx"].max(),
-        "maxy": gdf["maxy"].max(),
-    }
-    bounds["bbox"] = (bounds["minx"], bounds["miny"], bounds["maxx"], bounds["maxy"])
-    bounds["polygon"] = [
-        [bounds["minx"], bounds["miny"]],
-        [bounds["minx"], bounds["maxy"]],
-        [bounds["maxx"], bounds["maxy"]],
-        [bounds["maxx"], bounds["miny"]],
-        [bounds["minx"], bounds["miny"]],
-    ]
+    gdf = get_datasets.load_gdf_inspire_land_parcels(path)
+    file_bounds = gdf["geometry"].total_bounds
 
-    return bounds
+    bounds = {
+        "minx": file_bounds[0],
+        "miny": file_bounds[1],
+        "maxx": file_bounds[2],
+        "maxy": file_bounds[3],
+    }
+
+    bbox_polygon = shapely.Polygon(
+        [  # TODO: needs converting to BNG
+            [bounds["minx"], bounds["miny"]],
+            [bounds["minx"], bounds["maxy"]],
+            [bounds["maxx"], bounds["maxy"]],
+            [bounds["maxx"], bounds["miny"]],
+            [bounds["minx"], bounds["miny"]],
+        ]
+    )
+
+    return bbox_polygon
+
+
+def transform_gdf_land_parcels(inspire_file):
+    """ """
+    gdf = get_datasets.load_gdf_inspire_land_parcels(inspire_file)
+    gdf = gdf[["NATIONALCADASTRALREFERENCE", "geometry"]]
+    gdf = geo_utils.transform_gdf_drop_close_duplicates(gdf)
+    gdf["land_area"] = gdf["geometry"].area
+
+    return gdf
