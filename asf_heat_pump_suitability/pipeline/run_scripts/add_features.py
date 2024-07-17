69738dc359f139615ed869ab32ec49b7911e3c34
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-07-17 15:35:38 +0100

    remove unused line in add_features.py script

--- asf_heat_pump_suitability/pipeline/run_scripts/add_features.py
+++ asf_heat_pump_suitability/pipeline/run_scripts/add_features.py
@@ -65,7 +65,6 @@ def main(epc_path: str, save_output: Optional[str] = None) -> pl.DataFrame:
     # Add feature: lat/long
     logging.info("Adding lat/lon data to EPC")
     uprn_latlon_df = lat_lon.transform_df_osopen_uprn_latlon()
-    epc_latlon_df = epc_df.select(["UPRN"])
 
     # Join enhanced datasets together
     enhanced_epc_df = enhanced_epc_df.join(uprn_latlon_df, how="left", on="UPRN")
