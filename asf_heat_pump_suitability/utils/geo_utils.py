1cb41f63e833210d9109837e36db4fcbe765c2ca
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-07-17 15:14:05 +0100

    add geo_utils.py and save_utils.py file with utils functions

--- asf_heat_pump_suitability/utils/geo_utils.py
+++ asf_heat_pump_suitability/utils/geo_utils.py
@@ -1,21 +1,65 @@
 import logging
+import geopandas as gpd
+import shapely
 
 
-def transform_gdf_drop_close_duplicates(
-    gdf,
-):  # TODO: this is dropping duplicate geometries within one file. Is there a reason we shouldnt do this?
+def transform_gdf_drop_duplicates(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     """
-    Drop polygons with the same representative point. This should drop duplicates and almost duplicates
+    Drop polygons with the same representative point. This drops both duplicate and nearly identical geometries.
+
+    Args:
+        gdf (gpd.GeoDataFrame): GeoDataFrame with polygon geometries
+
+    Returns:
+        gpd.GeoDataFrame: GeoDataFrame with duplicate polygon geometries dropped
     """
+    # TODO: this assumes duplicate polygons will always generate the same representative points. Is this correct?
     gdf["rep_point"] = gdf.representative_point()
-    if not gdf["rep_point"].nunique() == len(gdf):
-        dups_count = gdf.duplicated(subset="rep_point").sum()
+    if gdf["rep_point"].nunique() != len(gdf):
+        duplicate_count = gdf.duplicated(subset="rep_point").sum()
+
+        logging.info(
+            f"Polygons containing same representative point found. "
+            f"Dropping {duplicate_count} polygons."
+        )
+
+        # Sort values to create replicable duplicate removal process
         gdf = gdf.sort_values(by="geometry").drop_duplicates(
             subset="rep_point", keep="first"
         )
-        logging.info(
-            f"Polygons containing same representative point found. "
-            f"Dropping {dups_count} polygons."
-        )
+
+    gdf = gdf.drop("rep_point", axis=1)
 
     return gdf
+
+
+def get_polygon_gdf_bounds(gdf: gpd.GeoDataFrame) -> shapely.Polygon:
+    """
+    Get bounding polygon of GeoDataFrame.
+
+    Args:
+        gdf (gpd.GeoDataFrame): GeoDataFrame
+
+    Returns:
+        shapely.Polygon: bounding polygon of GeoDataFrame
+    """
+    gdf_bounds = gdf["geometry"].total_bounds
+
+    bounds = {
+        "minx": gdf_bounds[0],
+        "miny": gdf_bounds[1],
+        "maxx": gdf_bounds[2],
+        "maxy": gdf_bounds[3],
+    }
+
+    bbox_polygon = shapely.Polygon(
+        [
+            [bounds["minx"], bounds["miny"]],
+            [bounds["minx"], bounds["maxy"]],
+            [bounds["maxx"], bounds["maxy"]],
+            [bounds["maxx"], bounds["miny"]],
+            [bounds["minx"], bounds["miny"]],
+        ]
+    )
+
+    return bbox_polygon
---
+++ asf_heat_pump_suitability/utils/save_utils.py
@@ -0,0 +1,18 @@
+import s3fs
+import polars as pl
+
+
+def save_df_as_parquet_to_s3(df: pl.DataFrame, path: str) -> None:
+    """
+    Save dataframe as parquet file to S3.
+
+    Args:
+        df (pl.DataFrame): dataframe
+        path (str): path to S3 destination
+
+    Returns:
+        None
+    """
+    fs = s3fs.S3FileSystem()
+    with fs.open(path=path, mode="wb") as f:
+        df.write_parquet(f)
