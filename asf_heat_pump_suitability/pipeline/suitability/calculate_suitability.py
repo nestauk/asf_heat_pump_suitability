a5796d4c56798658334ce761c40f62dabf90127e
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-12-04 18:20:44 +0000

    refactor calculate_suitability.py into function module, scoring.py contain score dicts, and run_calculate_suitability.py run script

--- asf_heat_pump_suitability/pipeline/suitability/calculate_suitability.py
+++ asf_heat_pump_suitability/pipeline/suitability/calculate_suitability.py
@@ -3,205 +3,7 @@ Functions to calculate suitability of different HP technologies.
 """
 
 import polars as pl
-import s3fs
-from datetime import datetime
-from tqdm import tqdm
-import argparse
-import logging
-from asf_heat_pump_suitability import config
-
-
-site_regs_scores = {
-    "ASHP_S": 0.25,
-    "ASHP_N": 0.25,
-    "GSHP_S": 0.25,
-    "GSHP_N": 0.25,
-    "SGL_S": 0.25,
-    "SGL_N": 0.25,
-    "HN_S": 0.25,
-    "HN_N": 0.25,
-}
-
-grid_capacity_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 0,
-    "GSHP_S": 1,
-    "GSHP_N": 0,
-    "SGL_S": 1,
-    "SGL_N": 0,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-epc_threshold_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 0,
-    "GSHP_S": 1,
-    "GSHP_N": 0,
-    "SGL_S": 1,
-    "SGL_N": 0,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-water_tank_space_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 1,
-    "GSHP_S": 1,
-    "GSHP_N": 1,
-    "SGL_S": 1,
-    "SGL_N": 1,
-    "HN_S": 1,
-    "HN_N": 1,
-}
-
-garden_size_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 0,
-    "GSHP_S": 1,
-    "GSHP_N": 0,
-    "SGL_S": 1,
-    "SGL_N": 0,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-external_space_scores = {
-    "ASHP_S": 0,
-    "ASHP_N": 2,
-    "GSHP_S": 0,
-    "GSHP_N": 1,
-    "SGL_S": 0,
-    "SGL_N": 0,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-offgas_scores = {
-    "ASHP_S": 0.5,
-    "ASHP_N": 0.5,
-    "GSHP_S": 0.5,
-    "GSHP_N": 0.5,
-    "SGL_S": 0.5,
-    "SGL_N": 0.5,
-    "HN_S": 0.5,
-    "HN_N": 0.5,
-}
-
-property_density_scores = {
-    "ASHP_S": 0,
-    "ASHP_N": 0,
-    "GSHP_S": 0,
-    "GSHP_N": 0,
-    "SGL_S": 2,
-    "SGL_N": 2,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-high_heat_demand_scores = {
-    "ASHP_S": 0,
-    "ASHP_N": 0,
-    "GSHP_S": 0,
-    "GSHP_N": 0,
-    "SGL_S": 0,
-    "SGL_N": 0,
-    "HN_S": 2,
-    "HN_N": 2,
-}
-
-anchor_properties_scores = {
-    "ASHP_S": 0,
-    "ASHP_N": 0,
-    "GSHP_S": 0,
-    "GSHP_N": 0,
-    "SGL_S": 0,
-    "SGL_N": 0,
-    "HN_S": 1,
-    "HN_N": 1,
-}
-
-not_flat_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 1,
-    "GSHP_S": 1,
-    "GSHP_N": 1,
-    "SGL_S": 0,
-    "SGL_N": 0,
-    "HN_S": 0,
-    "HN_N": 0,
-}
-
-multiple_props_scores = {
-    "ASHP_S": 0,
-    "ASHP_N": 0,
-    "GSHP_S": 0,
-    "GSHP_N": 0,
-    "SGL_S": 2,
-    "SGL_N": 2,
-    "HN_S": 2,
-    "HN_N": 2,
-}
-
-# TODO to refine
-grid_capacity_scores = {
-    "ASHP_S": 1,
-    "ASHP_N": 1,
-    "GSHP_S": 1,
-    "GSHP_N": 1,
-    "SGL_S": 1,
-    "SGL_N": 1,
-    "HN_S": 1,
-    "HN_N": 1,
-}
-
-
-def parse_arguments():
-    """
-    Create ArgumentParser and parse.
-
-    Returns:
-        argparse.Namespace: populated `Namespace`
-    """
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--epc_path",
-        help="Path to parquet with EPC properties with added features and weights for calculating suitability score from.",
-        required=True,
-    )
-
-    return parser.parse_args()
-
-
-def get_enhanced_epc(path) -> pl.DataFrame:
-    """
-    Load EPC dataset enhanced with weights and additional features.
-
-    Returns:
-        pl.DataFrame: enhanced EPC dataset
-    """
-    usecols = [
-        "UPRN",
-        "COUNTRY",
-        "lsoa",
-        "weight",
-        "proportional_weight",
-        "ruc_two_fold",
-        "off_gas",
-        "households_per_km2",
-        "garden_area_m2",
-        "listed_building_grade",
-        "in_conservation_area",
-        "lad_conservation_area_data_available",
-        "property_type",
-        "CURRENT_ENERGY_RATING",
-        "heatpump_installation_percentage",
-        "has_anchor_property",
-    ]
-    df = pl.read_parquet(path, columns=usecols)
-
-    return df
+from asf_heat_pump_suitability.pipeline.suitability import scoring
 
 
 def compute_df_avg_score_per_epc(
@@ -227,7 +29,7 @@ def compute_df_avg_score_per_epc(
     scores_df = compute_df_total_score_per_epc(
         df, tech_type, density_threshold, garden_threshold, external_space_threshold
     )
-    max_scores_df = compute_df_max_score_per_row(df, tech_type)
+    max_scores_df = compute_df_max_score_per_epc(df, tech_type)
     df = scores_df.join(max_scores_df, on="UPRN", how="inner")
     df = df.with_columns(
         (pl.col(f"{tech_type}_score") / pl.col(f"{tech_type}_max_score")).alias(
@@ -254,46 +56,47 @@ def compute_df_total_score_per_epc(
         density_threshold: minimum property density (households per km2) required for shared ground loop
         garden_threshold: minimum garden size (m2) required for heat pumps
         external_space_threshold: minimum outdoor space (m2) required for heat pumps
+        grid_installation_threshold: minimum percentage of properties in LSOA that could have a heat pump installed with current grid capacity
 
     Returns:
         pl.DataFrame: suitability score for specified tech type
     """
     df = df.with_columns(
         pl.when(pl.col("ruc_two_fold") == "Urban")
-        .then(high_heat_demand_scores.get(tech_type))
+        .then(scoring.high_heat_demand_scores.get(tech_type))
         .alias("heat_demand_score"),
-        pl.when(pl.col("OFF GAS"))
-        .then(offgas_scores.get(tech_type))
+        pl.when(pl.col("off_gas"))
+        .then(scoring.offgas_scores.get(tech_type))
         .alias("off_gas_score"),
         pl.when(pl.col("households_per_km2") > density_threshold)
-        .then(property_density_scores.get(tech_type))
+        .then(scoring.property_density_scores.get(tech_type))
         .alias("property_density_score"),
         pl.when(pl.col("garden_area_m2") > garden_threshold)
-        .then(garden_size_scores.get(tech_type))
+        .then(scoring.garden_size_scores.get(tech_type))
         .alias("garden_size_score"),
         pl.when(pl.col("garden_area_m2") > external_space_threshold)
-        .then(external_space_scores.get(tech_type))
+        .then(scoring.external_space_scores.get(tech_type))
         .alias("external_space_score"),
         pl.when(~pl.col("listed_building"))
-        .then(site_regs_scores.get(tech_type))
+        .then(scoring.site_regs_scores.get(tech_type))
         .alias("not_listed_score"),
-        pl.when(~pl.col("in_conservation_area"))
-        .then(site_regs_scores.get(tech_type))
-        .alias("not_in_cons_area_score"),
+        pl.when(~pl.col("in_protected_area"))
+        .then(scoring.site_regs_scores.get(tech_type))
+        .alias("not_in_protected_area_score"),
         pl.when(pl.col("property_type") != "Flat, maisonette or apartment")
-        .then(not_flat_scores.get(tech_type))
-        .otherwise(multiple_props_scores.get(tech_type))
+        .then(scoring.not_flat_scores.get(tech_type))
+        .otherwise(scoring.multiple_props_scores.get(tech_type))
         .alias("property_type_score"),
         pl.when(pl.col("CURRENT_ENERGY_RATING").is_in(["A", "B", "C"]))
-        .then(epc_threshold_scores.get(tech_type))
+        .then(scoring.epc_threshold_scores.get(tech_type))
         .alias("epc_rating_score"),
         pl.when(pl.col("has_anchor_property"))
-        .then(anchor_properties_scores.get(tech_type))
+        .then(scoring.anchor_properties_scores.get(tech_type))
         .alias("anchor_properties_score"),
         pl.when(
-            pl.col("heatpump_installation_percentage") > grid_installation_threshold
+            pl.col("heatpump_installation_percentage") >= grid_installation_threshold
         )
-        .then(epc_threshold_scores.get(tech_type))
+        .then(scoring.epc_threshold_scores.get(tech_type))
         .alias("grid_capacity_score"),
     )
 
@@ -303,10 +106,10 @@ def compute_df_total_score_per_epc(
     return df.select(["UPRN", f"{tech_type}_score"])
 
 
-def compute_df_max_score_per_row(df: pl.DataFrame, tech_type: str) -> pl.DataFrame:
+def compute_df_max_score_per_epc(df: pl.DataFrame, tech_type: str) -> pl.DataFrame:
     """
-    Get max score possible per row. This is calculated by adding together the scores for the tech type for each feature
-    in the row that is not null.
+    Get max score possible per EPC record. This is calculated by adding together the scores for the tech type for each
+    feature in the row that is not null.
 
     Args:
         df: EPC dataset with features for calculating suitability score
@@ -317,44 +120,50 @@ def compute_df_max_score_per_row(df: pl.DataFrame, tech_type: str) -> pl.DataFra
     """
     df = df.with_columns(
         pl.when(pl.col("ruc_two_fold").is_not_null())
-        .then(high_heat_demand_scores.get(tech_type))
+        .then(scoring.high_heat_demand_scores.get(tech_type))
         .otherwise(0)
         .alias("heat_demand_max"),
-        pl.when(pl.col("OFF GAS").is_not_null())
-        .then(offgas_scores.get(tech_type))
+        pl.when(pl.col("off_gas").is_not_null())
+        .then(scoring.offgas_scores.get(tech_type))
         .otherwise(0)
         .alias("off_gas_max"),
         pl.when(pl.col("households_per_km2").is_not_null())
-        .then(property_density_scores.get(tech_type))
+        .then(scoring.property_density_scores.get(tech_type))
         .otherwise(0)
         .alias("property_density_max"),
         pl.when(pl.col("garden_area_m2").is_not_null())
-        .then(garden_size_scores.get(tech_type) + external_space_scores.get(tech_type))
+        .then(
+            scoring.garden_size_scores.get(tech_type)
+            + scoring.external_space_scores.get(tech_type)
+        )
         .otherwise(0)
         .alias("garden_size_max"),
         pl.when(pl.col("listed_building").is_not_null())
-        .then(site_regs_scores.get(tech_type))
+        .then(scoring.site_regs_scores.get(tech_type))
         .alias("listed_buildings_max"),
-        pl.when(pl.col("in_conservation_area").is_not_null())
-        .then(site_regs_scores.get(tech_type))
-        .alias("in_cons_area_max"),
+        pl.when(pl.col("in_protected_area").is_not_null())
+        .then(scoring.site_regs_scores.get(tech_type))
+        .alias("in_protected_area_max"),
         pl.when(pl.col("property_type").is_not_null())
         .then(
             # Note: this only works because not_flat and multiple_props scores are mutually exclusive
-            (not_flat_scores.get(tech_type) + multiple_props_scores.get(tech_type))
+            (
+                scoring.not_flat_scores.get(tech_type)
+                + scoring.multiple_props_scores.get(tech_type)
+            )
         )
         .otherwise(0)
         .alias("property_type_max"),
         pl.when(pl.col("CURRENT_ENERGY_RATING").is_not_null())
-        .then(epc_threshold_scores.get(tech_type))
+        .then(scoring.epc_threshold_scores.get(tech_type))
         .otherwise(0)
         .alias("epc_rating_max"),
         pl.when(pl.col("has_anchor_properties").is_not_null())
-        .then(anchor_properties_scores.get(tech_type))
+        .then(scoring.anchor_properties_scores.get(tech_type))
         .otherwise(0)
         .alias("anchor_properties_max"),
         pl.when(pl.col("heatpump_installation_percentage").is_not_null())
-        .then(epc_threshold_scores.get(tech_type))
+        .then(scoring.epc_threshold_scores.get(tech_type))
         .otherwise(0)
         .alias("grid_capacity_max"),
     )
@@ -366,34 +175,25 @@ def compute_df_max_score_per_row(df: pl.DataFrame, tech_type: str) -> pl.DataFra
 
 
 def filter_df_minimum_features(
-    df: pl.DataFrame, features: list = None, threshold: int = 4
+    df: pl.DataFrame, features: list, threshold: int = 4
 ) -> pl.DataFrame:
     """
-    Calculate number of non-null features for each row of EPC.
+    Calculate number of non-null features for each row of EPC and filter for only rows with number of non-null features
+    above specified threshold.
 
     Args:
         df: EPC dataset with features.
         features: list of features to calculate HP suitability
-        threshold: minimum features required to be included
+        threshold: minimum features required to be included. Default 4.
+
+    Returns:
+        pl.DataFrame: EPC rows with required number of features
     """
-    if features is None:
-        features = [
-            "ruc_two_fold",
-            "OFF GAS",
-            "households_per_km2",
-            "garden_area_m2",
-            "listed_building_grade",
-            "in_conservation_area",
-            "property_type",
-            "CURRENT_ENERGY_RATING",
-            "has_anchor_properties",
-            "heatpump_installation_percentage",
-        ]
     df = df.with_columns(
         (len(features) - pl.sum_horizontal(pl.col(features).is_null()))
     ).rename(
         {"literal": "n_features"}
-    )  # Not sure why this is required, naming with alias directly doesnt work fsr
+    )  # Not sure why this is required, naming with alias directly doesn't work fsr
 
     df = df.filter(pl.col("n_features") >= threshold)
 
@@ -403,14 +203,14 @@ def filter_df_minimum_features(
 def compute_df_weighted_score(df, threshold=0.5):
     """
     Calculate [un]weighted suitability scores per EPC property in a single LSOA. Scores will only be weighted if the
-    proportion of EPC properties in the LSOA with non-null weight data is above the specified threshold.
+    proportion of EPC properties in the LSOA with weight data is above the specified threshold.
 
     Args:
-        df: EPC dataset for one LSOA with suitability scores per property for each tech type and with proportional weights
-        threshold (float): minimum proportion of properties in LSOA EPC sample with non-null weights
+        df: EPC dataset filtered to a single LSOA with suitability scores per property for each tech type and with proportional weights
+        threshold (float): minimum proportion of properties in LSOA EPC subset with weights. Default 0.5.
 
     Returns:
-        pl.DataFrame: weighted scores for an LSOA
+        pl.DataFrame: [un]weighted scores per EPC record for one LSOA
     """
     score_cols = [col for col in df.columns if "score" in col]
     df = df.with_columns(
@@ -418,7 +218,7 @@ def compute_df_weighted_score(df, threshold=0.5):
             (pl.col("proportional_weight").is_not_null().sum() / len(df)) >= threshold
         )
         .then(pl.col("proportional_weight") / pl.col("proportional_weight").sum())
-        .otherwise(1)
+        .otherwise(1)  # Otherwise we use a weight of 1 per row
         .alias("use_weight"),
         pl.when(
             (pl.col("proportional_weight").is_not_null().sum() / len(df)) >= threshold
@@ -437,14 +237,14 @@ def compute_df_weighted_score(df, threshold=0.5):
 
 def compute_dict_lsoa_suitability_scores(df: pl.DataFrame, lsoa: str) -> dict:
     """
-    Calculate average heat pump suitability scores for LSOA per tech type.
+    Calculate average heat pump suitability scores for one LSOA per tech type.
 
     Args:
         df (pl.DataFrame): LSOA with weighted suitability scores per tech type
         lsoa (str): LSOA code
 
     Returns:
-        dict: suitability scores per LSOA for each tech type
+        dict: suitability scores for one LSOA for each tech type
     """
     scores_dict = {"lsoa": lsoa}
     assert df["scores_weighted"].n_unique() == 1
@@ -458,67 +258,3 @@ def compute_dict_lsoa_suitability_scores(df: pl.DataFrame, lsoa: str) -> dict:
     scores_dict["n_properties"] = len(df)
 
     return scores_dict
-
-
-if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    args = parse_arguments()
-    # TODO: logging.info not displaying to terminal for me
-    logger.info("Loading EPC data with features")
-    epc_df = get_enhanced_epc(path=args.epc_path)
-
-    logger.info("Filtering EPC data to rows with n_features >= minimum threshold")
-    epc_df = filter_df_minimum_features(epc_df)
-
-    tech_types = [
-        "ASHP_S",
-        "ASHP_N",
-        "GSHP_S",
-        "GSHP_N",
-        "SGL_S",
-        "SGL_N",
-        "HN_S",
-        "HN_N",
-    ]
-
-    scores = []
-    for tech_type in tech_types:
-        logger.info(f"Calculating suitability scores for tech type: {tech_type}")
-        epc_scores_df = compute_df_avg_score_per_epc(epc_df, tech_type)
-        scores.append(epc_scores_df)
-
-    logger.info("Joining all scores to EPC dataset")
-    for score_df in scores:
-        epc_df = epc_df.join(score_df, on="UPRN", how="left")
-
-    fs = s3fs.S3FileSystem()
-    save_as = f"s3://asf-heat-pump-suitability/outputs/2023Q4/{datetime.today().strftime('%Y%m%d')}_2023_Q4_heat_pump_suitability_per_property.parquet"
-    with fs.open(save_as, mode="wb") as f:
-        epc_df.write_parquet(f)
-
-    logger.info("Weighting scores and aggregating per LSOA")
-    weighted_scores = []
-    for lsoa_code in tqdm(epc_df["lsoa"].unique()):
-        lsoa_df = epc_df.filter(pl.col("lsoa") == lsoa_code)
-        lsoa_df = compute_df_weighted_score(lsoa_df)
-        weighted_scores.append(compute_dict_lsoa_suitability_scores(lsoa_df, lsoa_code))
-    # Must have at least 15 properties to be included in score
-    suitability_df = pl.DataFrame(weighted_scores).filter(pl.col("n_properties") >= 15)
-    suitability_df = suitability_df.with_columns(pl.col(pl.Float64).round(3))
-
-    logger.info("Get LSOA names and join to suitability dataset")
-    lsoa_names_df = pl.read_csv(
-        config["data_source"]["EW_ons_lsoa_lad_lookup"],
-        columns=["LSOA21CD", "LSOA21NM"],
-    )
-    suitability_df = suitability_df.join(
-        lsoa_names_df, left_on="lsoa", right_on="LSOA21CD", how="left"
-    ).rename({"LSOA21NM": "lsoa_name"})
-
-    logger.info("Saving LSOA heat pump suitability scores")
-    fs = s3fs.S3FileSystem()
-    save_as = f"s3://asf-heat-pump-suitability/outputs/2023Q4/{datetime.today().strftime('%Y%m%d')}_2023_Q4_heat_pump_suitability_per_lsoa"
-    with fs.open(f"{save_as}.parquet", mode="wb") as f:
-        suitability_df.write_parquet(f)
-    with fs.open(f"{save_as}.csv", mode="wb") as f:
-        suitability_df.write_csv(f)
