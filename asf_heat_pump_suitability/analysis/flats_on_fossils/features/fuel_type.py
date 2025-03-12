3d28697a59b4353a80b7927abe0d7dd443cc5691
roisin <104171770+crispy-wonton@users.noreply.github.com>
2025-03-12 17:31:02 +0000

    update `fuel_type.py`: - refactor existing functions to extract fuel type to be more generic - add docstrings to functions

--- asf_heat_pump_suitability/analysis/flats_on_fossils/features/fuel_type.py
+++ asf_heat_pump_suitability/analysis/flats_on_fossils/features/fuel_type.py
@@ -2,19 +2,47 @@ import polars as pl
 
 
 def extend_df_fuel_type(df: pl.DataFrame) -> pl.DataFrame:
-    """ """
-    df = extract_col_main_fuel(df)
-    df = extract_col_mainheat_description(df).drop(
-        ["fill_fuel_type", "fill_community_heating_system"]
+    """
+    Add `fuel_type` and `community_heating` columns to EPC dataset using data in `MAIN_FUEL` and `MAINHEAT_DESCRIPTION`
+    columns. `fuel_type` contains central heating fuel type information, and `community_heating` contains boolean
+    values indicating whether the property is on a communal heating system or not.
+
+    Args:
+        df (pl.DataFrame): EPC dataset with `MAIN_FUEL` and `MAINHEAT_DESCRIPTION` columns
+
+    Returns:
+        pl.DataFrame: EPC dataset with `fuel_type` and `community_heating` columns
+    """
+    df = extend_col_fuel_type(df, epc_col="MAIN_FUEL", name="fuel_type")
+    df = extend_col_fuel_type(df, epc_col="MAINHEAT_DESCRIPTION", name="fill_fuel_type")
+    df = extend_col_communal_heating(df, "MAIN_FUEL", name="community_heating")
+    df = extend_col_communal_heating(
+        df, "MAINHEAT_DESCRIPTION", name="fill_community_heating"
     )
+
+    # Fill main fuel type and community heating columns
+    df = df.with_columns(
+        pl.col("fuel_type").fill_null(pl.col("fill_fuel_type")),
+        pl.col("community_heating").fill_null(pl.col("fill_community_heating")),
+    ).drop(["fill_fuel_type", "fill_community_heating"])
+
     df = extend_df_fossil_fuel_heating(df)
 
     return df
 
 
 def extend_df_fossil_fuel_heating(df: pl.DataFrame) -> pl.DataFrame:
-    """ """
-    fossil_fuels = ["gas", "oil", "coal", "LNG", "LPG"]
+    """
+    Add `fossil_fuel_heating` column to EPC dataset using derived `fuel_type` column. `fossil_fuel_heating` contains
+    boolean values to indicate whether the central heating uses fossil fuels or not.
+
+    Args:
+        df (pl.DataFrame): EPC dataset with derived `fuel_type` column
+
+    Returns:
+        pl.DataFrame: EPC dataset with `fossil_fuel_heating` column
+    """
+    fossil_fuels = ["gas", "oil", "coal", "LNG", "LPG", "B30"]
 
     df = df.with_columns(
         pl.when(pl.col("fuel_type").is_in(fossil_fuels))
@@ -28,131 +56,94 @@ def extend_df_fossil_fuel_heating(df: pl.DataFrame) -> pl.DataFrame:
     return df
 
 
-def extract_col_main_fuel(df: pl.DataFrame) -> pl.DataFrame:
-    """ """
+def extend_col_fuel_type(df: pl.DataFrame, epc_col: str, name: str) -> pl.DataFrame:
+    """
+    Process a string type EPC column containing information about central heating fuel to add a column containing fuel
+    type information.
+
+    Args:
+        df (pl.DataFrame): EPC dataset
+        epc_col (str): name of string type column in EPC data containing central heating fuel information
+        name (str): name of new column containing central heating fuel information
+
+    Returns:
+        pl.DataFrame: EPC dataset with new column containing central heating fuel type
+    """
     # Extract main fuel information into smaller number of unique values
     df = df.with_columns(
-        pl.when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("mains gas"))
+        # FOSSIL FUELS
+        pl.when(pl.col(epc_col).str.to_lowercase().str.contains("mains gas"))
         .then(pl.lit("gas"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("oil"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("oil"))
         .then(pl.lit("oil"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("lng"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("lng"))
         .then(pl.lit("LNG"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("electric"))
-        .then(pl.lit("electricity"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("biofuel"))
-        .then(pl.lit("biofuel"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("biogas"))
-        .then(pl.lit("biogas"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("lpg"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("lpg"))
         .then(pl.lit("LPG"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("b30"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("b30"))
         .then(pl.lit("B30"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("coal"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("coal"))
         .then(pl.lit("coal"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("biomass"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("anthracite"))
+        .then(pl.lit("coal"))
+        # RENEWABLES
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("electric"))
+        .then(pl.lit("electricity"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("heat pump"))
+        .then(pl.lit("electricity"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("biofuel"))
+        .then(pl.lit("biofuel"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("biogas"))
+        .then(pl.lit("biogas"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("biomass"))
         .then(pl.lit("biomass"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("waste combustion"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("waste"))
         .then(pl.lit("waste combustion"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("wood"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("wood"))
         .then(pl.lit("wood"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("anthracite"))
-        .then(pl.lit("coal"))
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains("bioethanol"))
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("bioethanol"))
         .then(pl.lit("bioethanol"))
+        # NO HEATING
         .when(
-            pl.col("MAIN_FUEL")
+            pl.col(epc_col)
             .str.to_lowercase()
             .str.contains("there is no heating/hot-water system")
         )
         .then(pl.lit("no heating"))
         .otherwise(None)
-        .alias("fuel_type"),
-        pl.when(
-            pl.col("MAIN_FUEL").str.to_lowercase()
-            == (r"no heating\/hot-water system or data is from a community network")
-        )
-        .then(None)
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains(r"\(community\)"))
-        .then(True)
-        .when(
-            pl.col("MAIN_FUEL")
-            .str.to_lowercase()
-            .str.contains("community heating schemes")
-        )
-        .then(True)
-        .when(pl.col("MAIN_FUEL").str.to_lowercase().str.contains(r"\(not community\)"))
-        .then(False)
-        .otherwise(None)
-        .alias("community_heating_system"),
+        .alias(name)
     )
 
     return df
 
 
-def extract_col_mainheat_description(df: pl.DataFrame) -> pl.DataFrame:
-    """ """
-    # Fill any missing fuel type or community heating system values with MAINHEAT_DESCRIPTION where possible
+def extend_col_communal_heating(
+    df: pl.DataFrame, epc_col: str, name: str
+) -> pl.DataFrame:
+    """
+    Process a string type EPC column containing information about central heating type to add a boolean column
+    to indicate whether a property uses communal heating systems or not.
+
+    Args:
+        df (pl.DataFrame): EPC dataset
+        epc_col (str): name of string type column in EPC data containing central heating information
+        name (str): name of new column containing communal heating boolean values
+
+    Returns:
+        pl.DataFrame: EPC dataset with new column containing communal heating boolean values
+    """
     df = df.with_columns(
         pl.when(
-            pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("mains gas")
+            pl.col(epc_col).str.to_lowercase()
+            == (r"no heating\/hot-water system or data is from a community network")
         )
-        .then(pl.lit("gas"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("lng"))
-        .then(pl.lit("LNG"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("oil"))
-        .then(pl.lit("oil"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("biofuel"))
-        .then(pl.lit("biofuel"))
-        .when(
-            pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("electric")
-        )
-        .then(pl.lit("electricity"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("biogas"))
-        .then(pl.lit("biogas"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("LPG"))
-        .then(pl.lit("LPG"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("b30"))
-        .then(pl.lit("B30"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("coal"))
-        .then(pl.lit("coal"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("biomass"))
-        .then(pl.lit("biomass"))
-        .when(
-            pl.col("MAINHEAT_DESCRIPTION")
-            .str.to_lowercase()
-            .str.contains("waste combustion")
-        )
-        .then(pl.lit("waste combustion"))
-        .when(pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("wood"))
-        .then(pl.lit("wood"))
-        .when(
-            pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("anthracite")
-        )
-        .then(pl.lit("coal"))
-        .when(
-            pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("bioethanol")
-        )
-        .then(pl.lit("bioethanol"))
-        .when(
-            pl.col("MAINHEAT_DESCRIPTION")
-            .str.to_lowercase()
-            .str.contains("there is no heating/hot-water system")
-        )
-        .then(pl.lit("no heating"))
-        .otherwise(None)
-        .alias("fill_fuel_type"),
-        pl.when(
-            pl.col("MAINHEAT_DESCRIPTION").str.to_lowercase().str.contains("community")
-        )
+        .then(None)
+        .when(pl.col(epc_col).str.to_lowercase().str.contains(r"\(not community\)"))
+        .then(False)
+        .when(pl.col(epc_col).str.to_lowercase().str.contains("community"))
         .then(True)
         .otherwise(None)
-        .alias("fill_community_heating_system"),
-    ).with_columns(
-        pl.col("fuel_type").fill_null(pl.col("fill_fuel_type")),
-        pl.col("community_heating_system").fill_null(
-            pl.col("fill_community_heating_system")
-        ),
+        .alias(name)
     )
 
     return df
