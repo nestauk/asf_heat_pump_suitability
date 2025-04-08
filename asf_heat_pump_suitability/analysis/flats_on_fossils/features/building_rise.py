d7d84c4b95bcd17e08282ef839303ba6f77f5375
roisin <104171770+crispy-wonton@users.noreply.github.com>
2025-04-08 10:41:08 +0100

    remove all alternative functions to add storey count / building rise and add one function to extend df with building rise information from storey count in `building_rise.py`

--- asf_heat_pump_suitability/analysis/flats_on_fossils/features/building_rise.py
+++ asf_heat_pump_suitability/analysis/flats_on_fossils/features/building_rise.py
@@ -35,181 +35,30 @@ def deduplicate_df_epc_building_footprints(df: pl.DataFrame) -> pl.DataFrame:
     return df.unique(subset="UPRN", keep="none")
 
 
-def calculate_dict_rise_thresholds(df: pl.DataFrame) -> dict:
-    """
-    Calculate median meters per storey per building rise type, and thresholds for minimum height of mid-rises and high-rises.
-
-    Args:
-        df (pl.DataFrame): EPC flat records with `FLAT_STOREY_COUNT` and building `height` variables
-
-    Returns:
-        dict: meters per storey for each building rise type and threshold for min height of mid- and high-rises
-    """
-    df = df.filter(
-        (pl.col("height").is_not_null()) & (pl.col("FLAT_STOREY_COUNT").is_not_null())
-    ).with_columns(
-        (pl.col("height") / pl.col("FLAT_STOREY_COUNT")).alias("meters_per_storey"),
-        pl.when((pl.col("FLAT_STOREY_COUNT") > 0) & (pl.col("FLAT_STOREY_COUNT") <= 3))
-        .then(pl.lit("low-rise"))
-        .when((pl.col("FLAT_STOREY_COUNT") > 3) & (pl.col("FLAT_STOREY_COUNT") <= 10))
-        .then(pl.lit("medium-rise"))
-        .when(pl.col("FLAT_STOREY_COUNT") > 10)
-        .then(pl.lit("high-rise"))
-        .otherwise(None)
-        .alias("building_rise"),
-    )
-
-    low_rise_uq = df.filter(pl.col("building_rise") == "low-rise")["height"].quantile(
-        0.75
-    )
-    mid_rise_lq = df.filter(pl.col("building_rise") == "medium-rise")[
-        "height"
-    ].quantile(0.25)
-
-    mid_rise_uq = df.filter(pl.col("building_rise") == "medium-rise")[
-        "height"
-    ].quantile(0.75)
-    high_rise_lq = df.filter(pl.col("building_rise") == "high-rise")["height"].quantile(
-        0.25
-    )
-
-    median_df = df.group_by("building_rise").agg(pl.col("meters_per_storey").median())
-    thresholds = {
-        "meters_per_storey": dict(
-            zip(median_df["building_rise"], median_df["meters_per_storey"])
-        )
-    }
-
-    thresholds.update(
-        {
-            "rise_thresholds": {
-                "mid_rise_min": np.mean([low_rise_uq, mid_rise_lq]),
-                "high_rise_min": np.mean([mid_rise_uq, high_rise_lq]),
-            }
-        }
-    )
-
-    return thresholds
-
-
-def extend_df_building_rise(
-    df: pl.DataFrame, mid_rise_min: float, high_rise_min: float, mps: dict
+def extend_df_building_rise_from_storey_count(
+    df: pl.DataFrame, storey_col: str, building_rise_col: str
 ) -> pl.DataFrame:
     """
-    Add `building_rise` column to dataframe using `FLAT_STOREY_COUNT` and `height` columns. Buildings are partitioned
+    Add new column to dataframe with building rise classification derived from storey count. Buildings are classified
     into low- (<=3 storeys), medium- (4-10 storeys) and high-rise (>10 storeys).
 
     Args:
-        df (pl.DataFrame): EPC records with `FLAT_STOREY_COUNT` and building `height` data
-        mid_rise_min (float): minimum building height of medium rises in meters
-        high_rise_min (float): minimum building height of high rises in meters
-        mps (dict): meters per storey used to calculate storey counts from building height
+        df (pl.DataFrame): dataset with storey count per record
+        storey_col (str): name of column containing storey count data
+        building_rise_col (str): name of new column containing building rise classification
 
     Returns:
-        pl.DataFrame: EPC data with `building_rise` column
+        pl.DataFrame: dataframe with building rise classification
     """
-    df = clean_col_flat_storey_count(df)
-    df = (
-        df.with_columns(
-            # Use flat storey count first
-            pl.when(
-                (pl.col("FLAT_STOREY_COUNT") > 0) & (pl.col("FLAT_STOREY_COUNT") <= 3)
-            )
-            .then(pl.lit("low-rise"))
-            .when(
-                (pl.col("FLAT_STOREY_COUNT") > 3) & (pl.col("FLAT_STOREY_COUNT") <= 10)
-            )
-            .then(pl.lit("medium-rise"))
-            .when(pl.col("FLAT_STOREY_COUNT") > 10)
-            .then(pl.lit("high-rise"))
-            # Then use building height data
-            .when(pl.col("height") >= high_rise_min)
-            .then(pl.lit("high-rise"))
-            .when(pl.col("height") >= mid_rise_min)
-            .then(pl.lit("medium-rise"))
-            .when(pl.col("height").is_not_null())
-            .then(pl.lit("low-rise"))
-            .otherwise(None)
-            .alias("building_rise")
-        )
-        .with_columns(
-            pl.col("building_rise")
-            .replace(mps)
-            .cast(pl.Float64)
-            .alias("meters_per_storey")
-        )
-        .with_columns(
-            (pl.col("height") / pl.col("meters_per_storey"))
-            .round()
-            .alias("derived_storey_count")
-        )
-        .with_columns(
-            pl.col("FLAT_STOREY_COUNT")
-            .fill_null(pl.col("derived_storey_count"))
-            .alias("storey_count")
-        )
-    )
-
-    return df
-
-
-def other_extend_df_building_rise(
-    df: pl.DataFrame, mps: float, roof_height: float = 2.3
-) -> pl.DataFrame:
-    """
-    Add `building_rise` column to dataframe using `FLAT_STOREY_COUNT` and `height` columns. Buildings are partitioned
-    into low- (<=3 storeys), medium- (4-10 storeys) and high-rise (>10 storeys).
-
-    Args:
-        df (pl.DataFrame): EPC records with `FLAT_STOREY_COUNT` and building `height` data
-        mps (float): meters per storey used to calculate storey counts from building height
-        roof_height (float): constant roof height in meters to use in calculation. Default 2.3 which is the national
-        regulation minimum ceiling height in the UK.
-
-    Returns:
-        pl.DataFrame: EPC data with `building_rise` column
-    """
-    df = clean_col_flat_storey_count(df)
-    df = extend_df_building_storey_count(df, mps, roof_height)
-
     df = df.with_columns(
-        pl.col("calculated_storey_count")
-        .fill_null(pl.col("FLAT_STOREY_COUNT"))
-        .alias("storey_count")
-    ).with_columns(
-        pl.when((pl.col("storey_count") > 0) & (pl.col("storey_count") <= 3))
+        pl.when((pl.col(storey_col) > 0) & (pl.col(storey_col) <= 3))
         .then(pl.lit("low-rise"))
-        .when((pl.col("storey_count") > 3) & (pl.col("storey_count") <= 10))
+        .when((pl.col(storey_col) > 3) & (pl.col(storey_col) <= 10))
         .then(pl.lit("medium-rise"))
-        .when(pl.col("storey_count") > 10)
+        .when(pl.col(storey_col) > 10)
         .then(pl.lit("high-rise"))
-        .otherwise(None)
-        .alias("building_rise")
+        .alias(building_rise_col)
     )
-
-    return df
-
-
-def extend_df_building_storey_count(
-    df: pl.DataFrame, mps: float, roof_height: float
-) -> pl.DataFrame:
-    """
-    Calculate storey count of a building by dividing building height by `mps` value.
-
-    Args:
-        df (pl.DataFrame): dataset with `height` column
-        mps (float): meters per storey used to calculate storey counts from building height
-        roof_height (float): constant roof height in meters to use in calculation
-
-    Returns:
-        pl.DataFrame: dataframe with `calculated_storey_count` column
-    """
-    df = df.with_columns(
-        ((pl.col("height") - roof_height) / mps)
-        .round()
-        .alias("calculated_storey_count")
-    )
-
     return df
 
 
