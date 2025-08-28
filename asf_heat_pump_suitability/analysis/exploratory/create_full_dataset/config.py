2572f0b73eda69b9b6f70e5a786a824f060e4582
sofiapinto <anasbpinto3@gmail.com>
2025-08-28 16:07:50 +0100

    adding city centre OAs to config

--- asf_heat_pump_suitability/analysis/exploratory/create_full_dataset/config.py
+++ asf_heat_pump_suitability/analysis/exploratory/create_full_dataset/config.py
@@ -58,3 +58,49 @@ weights = {
         # "close_to_city_center": 1
     },
 }
+
+# OAs in the city centre of Plymouth
+# Considering "City Centre, Barbican and Sutton Harbour" MSOA as the city center
+# OAs can be found using https://www.ons.gov.uk/explore-local-statistics/areas/E02003148-plymouth-027
+city_centre_oas = {
+    # OAs in LSOA Plymouth 027A
+    "E00076029",
+    "E00076042",
+    "E00076047",
+    "E00076048",
+    # OAs in LSOA Plymouth 027B
+    "E00076028",
+    "E00172050",
+    "E00181096",
+    "E00181180",
+    "E00181183",
+    # OAs in LSOA Plymouth 027C
+    "E00076542",
+    "E00076546",
+    "E00076552",
+    "E00076556",
+    "E00076558",
+    "E00076560",
+    "E00181118",
+    "E00181147",
+    # OAs in LSOA Plymouth 027E
+    "E00076559",
+    "E00076573",
+    "E00076574",
+    "E00076577",
+    "E00076579",
+    "E00076583",
+    # OAs in LSOA Plymouth 027F
+    "E00076553",
+    "E00076564",
+    "E00172052",
+    "E00172065",
+    "E00172069",
+    # OAs in LSOA Plymouth 027G
+    "E00076568",
+    "E00076569",
+    "E00076571",
+    "E00172054",
+    "E00172055",
+    "E00172056",
+}
