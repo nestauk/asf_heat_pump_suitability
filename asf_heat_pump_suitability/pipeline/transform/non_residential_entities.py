7a459ca813cf22be2857e0b1c1d264c98c849773
roisin <104171770+crispy-wonton@users.noreply.github.com>
2025-10-20 16:39:42 +0100

    add functions to identify non residential entities

---
+++ asf_heat_pump_suitability/pipeline/transform/non_residential_entities.py
@@ -0,0 +1,65 @@
+import geopandas as gpd
+import pandas as pd
+
+# TODO these may need refinement for large cities where overlap with residential is possible
+NO_RESIDENTIAL_OVERLAP_BUILDING_TYPES = [
+    "Port Consisting of Docks and Nautical Berthing",
+    "Fire Station",
+    "Hospital",
+    "Non State Secondary Education",
+    "Secondary Education",
+    "Higher or University Education",
+    "Primary Education",
+    "Post Office",
+    "Place Of Worship",
+    "Medical Care Accommodation",
+    "Museum",
+    "Special Needs Education",
+    "Further Education",
+    "Non State Primary Education",
+    "Coach Station",
+    "Police Station",
+    "Sports And Leisure Centre",
+    "Vehicular Ferry Terminal",
+    "Hospice",
+    "Bus Station",
+]
+
+
+def transform_gdf_non_residential_buildings(
+    important_building_gdf: gpd.GeoDataFrame,
+    railway_station_gdf: gpd.GeoDataFrame,
+    building_gdf: gpd.GeoDataFrame,
+) -> gpd.GeoDataFrame:
+    """
+    Transform important buildings and railway data to create a dataframe of polygons representing buildings
+    which are unlikely to contain residential properties, e.g. hospitals, train stations, museums etc.
+
+    Args:
+        important_building_gdf (gpd.GeoDataFrame): OS OpenMap Local important building footprints in area of interest
+        railway_station_gdf (gpd.GeoDataFrame): OS OpenMap Local railway station point geometries in area of interest
+        building_gdf (gpd.GeoDataFrame): all building footprints in area of interest
+
+    Returns:
+        gpd.GeoDataFrame: geometries of buildings which are unlikely to contain residential properties
+    """
+    print("Creating non-residential buildings dataset...")
+    # Assert all gdfs have the same CRS
+    assert (
+        len({important_building_gdf.crs, railway_station_gdf.crs, building_gdf.crs})
+        == 1
+    )
+
+    # Get buildings which are unlikely to have residential overlap
+    exclude_buildings_gdf = important_building_gdf[
+        important_building_gdf["CLASSIFICA"].isin(NO_RESIDENTIAL_OVERLAP_BUILDING_TYPES)
+    ]
+
+    # Get buildings which are railway stations (railway stations are only given as point geometries)
+    railway_station_gdf = building_gdf.sjoin(
+        railway_station_gdf, how="inner", predicate="contains"
+    )
+
+    return pd.concat(
+        [exclude_buildings_gdf[["geometry"]], railway_station_gdf[["geometry"]]]
+    )
