24ac58ea10f477e2db4a3cc0418a0a701ad55422
roisin <104171770+crispy-wonton@users.noreply.github.com>
2025-10-28 14:29:09 +0000

    update getter for OS Open UPRN data

---
+++ asf_heat_pump_suitability/getters/load_geodata.py
@@ -0,0 +1,30 @@
+import polars as pl
+import regex as re
+import os
+
+from asf_heat_pump_suitability import config
+from asf_heat_pump_suitability.getters import base_getters
+
+
+def load_df_osopen_uprn(**kwargs) -> pl.DataFrame:
+    """
+    Get raw OS (Ordnance Survey) Open UPRN dataset containing latitude and longitude and British National Grid X and Y
+    coordinates for all UPRNs in Great Britain.
+
+    Args:
+        **kwargs fo pl.read_csv
+
+    Returns:
+        pl.DataFrame: raw OS Open UPRN dataset with lat/lon and x/y coordinates for every UPRN
+    """
+    print("Loading OSOpen UPRNs...")
+    path = config["data"]["geodata"]["uk_osopen_uprn"]
+    # extract_file = re.search(r"osopenuprn_\d{6}", path).group()
+    extract_file = os.path.basename(path).split("_csv")[0]
+    df = base_getters.get_df_from_zip_csv_s3(
+        path,
+        extract_file=f"{extract_file}.csv",
+        **kwargs,
+    )
+
+    return df
