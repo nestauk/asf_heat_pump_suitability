f5a454a674f48993333c03c2154e36f32b2dd432
roisin <104171770+crispy-wonton@users.noreply.github.com>
2025-12-02 11:56:23 +0000

    rename & move garden_size.py and update to retain total and max contiguous outdoor space

---
+++ asf_heat_pump_suitability/pipeline/transform/outdoor_space.py
@@ -0,0 +1,159 @@
+import geopandas as gpd
+import logging
+import pandas as pd
+import polars as pl
+
+
+def match_series_files_land_building(
+    land_files_gdf: gpd.GeoDataFrame, building_files_gdf: gpd.GeoDataFrame
+) -> pd.Series:
+    """
+    Get Series of intersecting INSPIRE land parcel files and Microsoft building footprint files.
+
+    Args:
+        land_files_gdf (gpd.GeoDataFrame): INSPIRE land file names and file bounding polygons
+        building_files_gdf (gpd.GeoDataFrame): Microsoft building footprint file names and file bounding polygons
+
+    Returns:
+        pd.Series: Series where indices are INSPIRE land parcel file names and values are Microsoft building footprint files
+        names
+    """
+    logging.info("Mapping building footprint files to INSPIRE land registry files")
+    gdf = land_files_gdf.sjoin(building_files_gdf, how="inner", predicate="intersects")
+    file_matches = pd.Series(
+        gdf["ms_url"].values, index=gdf["inspire_file_name"]
+    ).sort_index()
+
+    return file_matches
+
+
+def generate_gdf_building_intersections(
+    land_parcels_gdf: gpd.GeoDataFrame,
+    building_footprints_gdf: gpd.GeoDataFrame,
+    outbuilding_size: int = 30,
+    s_building_prop: float = 0.45,
+    min_intersection: float = 15,
+) -> gpd.GeoDataFrame:
+    """
+    Generate intersections between land parcel polygons and building footprint polygons, with rules applied to remove
+    suspected erroneous building intersections (i.e. small corners or slivers of buildings) but retain outbuildings.
+    Building intersections are kept if they meet the following conditions:
+    1. building intersection area <= outbuilding_size and >= s_building_prop * outbuilding_size
+    2. building intersection area > outbuilding_size and building intersection area >= min_intersection
+
+    Args:
+        land_parcels_gdf (gpd.GeoDataFrame): land parcel polygons
+        building_footprints_gdf (gpd.GeoDataFrame): building footprint polygons
+        outbuilding_size (int): max area (m2) of building footprints assumed to be outbuildings. Default 30m2.
+        s_building_prop (float): minimum proportion of buildings smaller than outbuilding_size. Default 45%.
+        min_intersection (float): minimum area of intersection of buildings larger than outbuilding_size (metres squared). Default 15m2.
+
+    Returns:
+        gpd.GeoDataFrame: intersections of land parcel polygons and building footprint polygons
+    """
+    gdf = gpd.overlay(
+        land_parcels_gdf,
+        building_footprints_gdf,
+        how="intersection",
+        keep_geom_type=False,
+    )
+    gdf["building_intersection_area_m2"] = gdf["geometry"].area
+    gdf["min_size_of_small_building"] = gdf["building_area_m2"] * s_building_prop
+
+    gdf = gdf[
+        (
+            (gdf["building_area_m2"] <= outbuilding_size)
+            & (
+                gdf["building_intersection_area_m2"]
+                >= gdf["min_size_of_small_building"]
+            )
+        )
+        | (
+            (gdf["building_area_m2"] > outbuilding_size)
+            & (gdf["building_intersection_area_m2"] >= min_intersection)
+        )
+    ]
+
+    return gdf
+
+
+def generate_gdf_outdoor_space(
+    building_intersections_gdf: gpd.GeoDataFrame, land_parcels_gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame:
+    """
+    Generate GeoDataFrame of land extents with total and max contiguous outdoor space areas.
+
+    Args:
+        building_intersections_gdf (gpd.GeoDataFrame): intersections of land parcel polygons and building footprint polygons
+        land_parcels_gdf (gpd.GeoDataFrame): land parcel polygons
+
+    Returns:
+        gpd.GeoDataFrame: land parcels with total and max contiguous outdoor space area (m2)
+    """
+    # Get land minus buildings - MultiPolygons will be created for land parcels which get split into multi-parts
+    land_minus_buildings = gpd.overlay(
+        land_parcels_gdf,
+        building_intersections_gdf,
+        how="difference",
+        keep_geom_type=False,
+    )
+
+    # Explode multi-part geometries into single part geometries to get largest piece
+    land_minus_buildings_parts = land_minus_buildings.explode(index_parts=False)
+    land_minus_buildings_parts["outdoor_space_area_m2"] = (
+        land_minus_buildings_parts.geometry.area
+    )
+
+    # Keep max size and total outdoor area
+    outdoor_space_df = (
+        land_minus_buildings_parts.groupby("NATIONALCADASTRALREFERENCE")
+        .agg(
+            max_contiguous_outdoor_space_area_m2=("outdoor_space_area_m2", max),
+            total_outdoor_space_area_m2=("outdoor_space_area_m2", sum),
+        )
+        .reset_index(
+            # Drop duplicates in case there are multiple max values
+        )
+        .drop_duplicates(subset="NATIONALCADASTRALREFERENCE")
+    )
+
+    # Merge full land extent geometry back on
+    outdoor_space_gdf = outdoor_space_df.merge(
+        land_parcels_gdf, how="left", on="NATIONALCADASTRALREFERENCE"
+    )
+
+    return outdoor_space_gdf
+
+
+def deduplicate_df_outdoor_space(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Deduplicate UPRNs matched to multiple land extents by keeping the one with the smallest total area.
+
+    Args:
+        df (pl.DataFrame): UPRNs with outdoor space estimates
+
+    Returns:
+        pl.DataFrame: deduplicated UPRNs with outdoor space estimates
+    """
+    df = df.with_columns(pl.col("UPRN").is_duplicated().alias("UPRN_duplicated"))
+
+    _deduplicated_df = (
+        df.filter(pl.col("UPRN_duplicated"))
+        .with_columns(
+            min_total=pl.col("total_outdoor_space_area_m2")
+            .min()
+            .over("NATIONALCADASTRALREFERENCE")
+        )
+        .filter(
+            # Get smallest outdoor space
+            pl.col("total_outdoor_space_area_m2")
+            == pl.col("min_total")
+            # Deduplicate - any duplicates will now (most likely) have the same total outdoor space
+        )
+        .unique(subset="NATIONALCADASTRALREFERENCE")
+        .drop("min_total")
+    )
+
+    return pl.concat([df.filter(~pl.col("UPRN_duplicated")), _deduplicated_df]).drop(
+        "UPRN_duplicated"
+    )
