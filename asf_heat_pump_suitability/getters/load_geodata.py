5e80e6206a0f8c5d562b790b42b5ee7c6ef4c6b9
eglucas <elysialucas@gmail.com>
2025-12-05 10:19:08 +0000

    add spatial signature data getter

--- asf_heat_pump_suitability/getters/load_geodata.py
+++ asf_heat_pump_suitability/getters/load_geodata.py
@@ -57,3 +57,39 @@ def load_gdf_heat_network_zones(local_authority: str, **kwargs) -> gpd.GeoDataFr
         f"Heat network zone geodataframe successfully loaded for {local_authority} with CRS {gdf.crs}."
     )
     return gdf
+
+
+def load_gdf_spatial_signatures_gb(
+    detail_level: str = "simplified", **kwargs
+) -> gpd.GeoDataFrame:
+    """
+    Load GeoDataFrame with polygons in GB from the Spatial Signatures Framework  classified by their
+    Spatial Signature type. (Source: https://doi.org/10.6084/m9.figshare.16691575).
+
+    The dataset can be loaded at two levels of detail:
+    - "simplified": geometries provided with only fields "id" and "type"
+    - "full": geometries provided with all fields
+
+    Args:
+        detail_level (str, optional): Which level of descriptive detail to load.
+            Must be either "simplified" or "full". Defaults to "simplified".
+
+    Returns:
+        gpd.GeoDataFrame: spatial signature polygons in GB.
+    """
+
+    if detail_level not in {"full", "simplified"}:
+        raise ValueError(
+            f"detail_level must be 'full' or 'simplified', not {detail_level}"
+        )
+
+    gdf = base_getters.get_gdf_from_gpkg_s3_path(
+        path=config["data"]["geodata"]["gb_spatial_signatures"][detail_level],
+        **kwargs,
+    )
+
+    print(
+        f"Spatial signatures {detail_level} geodataframe successfully loaded with CRS {gdf.crs}."
+    )
+
+    return gdf
