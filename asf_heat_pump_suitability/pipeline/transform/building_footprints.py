466cdf0c76fd05a8e1e3d0603cd0255684eeeea0
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-02-26 14:45:21 +0000

    fix bugs in building_footprints.py

--- asf_heat_pump_suitability/pipeline/transform/building_footprints.py
+++ asf_heat_pump_suitability/pipeline/transform/building_footprints.py
@@ -10,6 +10,13 @@ def generate_gdf_building_sections(
     keep_uprns: List[int] = None,
 ) -> gpd.GeoDataFrame:
     """ """
+    if not isinstance(boundary, shapely.Polygon) and not isinstance(
+        boundary, shapely.MultiPolygon
+    ):
+        raise TypeError("`boundary` argument must be a Polygon or MultiPolygon")
+
+    print("Splitting building footprints with Voronoi polygons...")
+
     if keep_uprns:
         voronoi_gdf = uprns_gdf[
             uprns_gdf.intersects(boundary) & uprns_gdf["UPRN"].isin(keep_uprns)
@@ -22,8 +29,10 @@ def generate_gdf_building_sections(
         .reset_index(drop=True)
         .rename(columns={"UPRN": "n_UPRNs"})
     )
-    voronoi_gdf["geometry"] = voronoi_gdf.voronoi_polygons(extend_to=boundary)
+    voronoi_gdf["geometry"] = voronoi_gdf.voronoi_polygons(
+        extend_to=boundary
+    ).make_valid()
     voronoi_gdf = voronoi_gdf.dropna(subset="geometry")
     voronoi_gdf = gpd.clip(voronoi_gdf, boundary)
 
-    return voronoi_gdf.overlay(buildings_gdf, how="intersection")
+    return voronoi_gdf.overlay(buildings_gdf, how="intersection", keep_geom_type=False)
