37c26007a56ac5f648da1a5d46c1ffd686d70bb2
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-02-26 18:05:51 +0000

    change UPRN to EPC join and restructure output data for example opportunity areas

--- asf_heat_pump_suitability/research/analysis/example_opportunity_areas_counts/plymouth_example_opportunity_area_counts.py
+++ asf_heat_pump_suitability/research/analysis/example_opportunity_areas_counts/plymouth_example_opportunity_area_counts.py
@@ -3,24 +3,25 @@ Script to use EPC data to get value counts of TENURE, ATTACHMENT TYPE, and EPC R
 3 example opportunity areas in Plymouth.
 
 To run:
-python asf_heat_pump_suitability/research/exploratory/example_opportunity_areas_counts/plymouth_example_opportunity_area_counts.py
+python asf_heat_pump_suitability/research/analysis/example_opportunity_areas_counts/plymouth_example_opportunity_area_counts.py
 """
 
 if __name__ == "__main__":
     import polars as pl
     import geopandas as gpd
+    from asf_heat_pump_suitability.utils import save_utils
     from asf_heat_pump_suitability.getters import load_geodata
     from asf_heat_pump_suitability.pipeline.transform import uprns
+
+    # Load UPRNs
+    uprns_df = load_geodata.load_df_osopen_uprn()
+    uprns_gdf = uprns.generate_gdf_uprn_coords(uprns_df)
 
     # Load EPC
     print("Loading deduplicated EPC data...")
     raw_epc_df = pl.read_parquet(
         "s3://asf-daps/lakehouse/2025_Q1/processed/epc/deduplicated/processed_dedupl-0.parquet"
     )
-
-    # Load UPRNs
-    uprns_df = load_geodata.load_df_osopen_uprn()
-    uprns_gdf = uprns.generate_gdf_uprn_coords(uprns_df)
 
     # Load opportunity areas
     print("Loading opportunity areas...")
@@ -35,7 +36,6 @@ if __name__ == "__main__":
         "BUILT_FORM",
         "TENURE",
         "CURRENT_ENERGY_RATING",
-        "ENERGY_RATING_CAT",
     ]
 
     print("Processing EPC data...")
@@ -48,9 +48,9 @@ if __name__ == "__main__":
             pl.col("UPRN")
             .cast(pl.Float64, strict=False)
             .cast(pl.Int64)
-            .alias("processed_UPRN")
+            .alias("UPRN")
         )
-        .drop_nulls(subset="processed_UPRN")
+        .drop_nulls(subset="UPRN")
         .with_columns(
             pl.col("PROPERTY_TYPE").cast(pl.String),
             pl.col("BUILT_FORM").cast(pl.String),
@@ -63,41 +63,50 @@ if __name__ == "__main__":
             .then(pl.lit("Mid-Terrace"))
             .when(pl.col("BUILT_FORM") == "Enclosed End-Terrace")
             .then(pl.lit("End-Terrace"))
-            .when(pl.col("BUILT_FORM") == "")
-            .then(pl.lit("unknown"))
+            .when(pl.col("BUILT_FORM").str.to_lowercase().is_in(["", "unknown"]))
+            .then(None)
             .otherwise(pl.col("BUILT_FORM"))
-            .alias("attachment")
+            .alias("ATTACHMENT")
+        )
+        .with_columns(
+            pl.col("TENURE")
+            .str.to_lowercase()
+            .replace({"": None, "unknown": None})
+            .alias("TENURE")
         )
     )
 
-    print("Joining geospatial coordinates to EPC data...")
-    # Add geospatial coordinates to EPC UPRNs
-    epc_gdf = uprns_gdf.merge(
-        epc_df.to_pandas(), how="inner", left_on="UPRN", right_on="processed_UPRN"
+    print("Joining EPC data to UPRNs...")
+    # Add EPC data to UPRNs
+    keep_cols = ["UPRN", "ATTACHMENT", "TENURE", "CURRENT_ENERGY_RATING"]
+    uprns_gdf = uprns_gdf.merge(epc_df.to_pandas(), how="left", on="UPRN").fillna(
+        "Unknown"
     )
 
     # Filter to UPRNs which are in opportunity areas
-    areas_epc_gdf = epc_gdf.sjoin(
-        areas_gdf[["Name", "geometry"]], how="inner", predicate="within"
-    ).drop(columns="index_right")
+    opportunity_areas_df = pl.from_pandas(
+        uprns_gdf.sjoin(
+            areas_gdf[["Name", "geometry"]], how="right", predicate="within"
+        ).drop(columns=["index_left", "geometry"])
+    )
 
-    fname = "plymouth_example_opportunity_areas_feature_counts.txt"
-    print(f"Saving outputs to {fname}")
-    # Save the value counts of features of interest to txt file
-    with open(fname, "w") as file:
-        for tech in areas_epc_gdf["Name"].unique():
-            _gdf = areas_epc_gdf[areas_epc_gdf["Name"] == tech].drop_duplicates(
-                subset="processed_UPRN"
-            )
-            file.write(f"\n\n{tech}: total properties: {len(_gdf)}")
+    print("Calculate value counts per feature...")
+    keep_cols += ["Name"]
+    dummy_cols = ["ATTACHMENT", "TENURE", "CURRENT_ENERGY_RATING"]
+    totals_df = opportunity_areas_df.group_by("Name").agg(
+        pl.col("UPRN").count().alias("n_UPRNs")
+    )
+    opportunity_areas_df = (
+        opportunity_areas_df.select(keep_cols)
+        .to_dummies(columns=dummy_cols)
+        .group_by("Name")
+        .agg(pl.all().sum())
+        .drop("UPRN")
+    )
 
-            file.write(f"\n\n{tech} tenure:")
-            file.write(_gdf["TENURE"].value_counts().to_string())
+    opportunity_areas_df = totals_df.join(
+        opportunity_areas_df, how="left", on="Name"
+    ).rename({"Name": "opportunity_area_code"})
 
-            file.write(f"\n\n{tech} attachment:")
-            file.write(_gdf["attachment"].value_counts().to_string())
-
-            file.write(f"\n\n{tech} EPC rating:")
-            file.write(
-                _gdf["CURRENT_ENERGY_RATING"].value_counts().sort_index().to_string()
-            )
+    path = "s3://asf-heat-pump-suitability/local_heat_planning/outputs/plymouth/plymouth_example_opportunity_areas_feature_counts.csv"
+    save_utils.save_to_s3(opportunity_areas_df, path)
