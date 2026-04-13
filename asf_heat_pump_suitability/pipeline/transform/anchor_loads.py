4e6b9a392353c1ca45212b520d6243ce4757e325
Sofia Pinto <anasbpinto3@gmail.com>
2026-04-13 10:54:36 +0100

    add new features

---
+++ asf_heat_pump_suitability/pipeline/transform/anchor_loads.py
@@ -0,0 +1,21 @@
+""" """
+
+ANCHOR_CATEGORIES = [
+    "Primary Education",
+    "Museum",
+    "Library",
+    "Further Education",
+    "Secondary Education",
+    "Fire Station",
+    "Sports And Leisure Centre",
+    "Hospital",
+    "Higher or University Education",
+    "Special Needs Education",
+    "Medical Care Accommodation",
+    "Non State Primary Education",
+    "Non State Secondary Education",
+    "Art Gallery",
+    "Police Station",
+    "Hospice",
+    "Airport",
+]
