0adebdbe655d0117c531057af513069e8bf2078b
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-04-15 17:44:38 +0100

    remove code for comparative analysis of pipeline and evaluation UPRNs

--- asf_heat_pump_suitability/research/exploratory/domestic_filtering/domestic_building_identification_threshold_selection.py
+++ asf_heat_pump_suitability/research/exploratory/domestic_filtering/domestic_building_identification_threshold_selection.py
@@ -48,167 +48,6 @@ def transform_gdf_council_tax(df: pd.DataFrame) -> gpd.GeoDataFrame:
     ).drop_duplicates(subset="UPRN")
 
 
-def calculate_dict_uprn_diffs_per_dataset(
-    raw_council_tax_df: pd.DataFrame,
-    council_tax_gdf: gpd.GeoDataFrame,
-    pipeline_gdf: gpd.GeoDataFrame,
-) -> dict:
-    """
-    Create a dictionary with a summary of the differences between UPRN counts and unique sets in the outputs of the domestic identification
-    pipeline versus the council tax evaluation dataset.
-
-    Args:
-        raw_council_tax_df (pd.DataFrame): raw unprocessed council tax data
-        council_tax_gdf (gpd.GeoDataFrame): processed council tax data with empty UPRN rows removed and with geopoints per UPRN
-        pipeline_gdf (gpd.GeoDataFrame): domestic UPRNs identified by current pipeline
-
-    Returns:
-        dict: summary of UPRN differences between pipeline and evaluation datasets
-    """
-    council_uprns = set(council_tax_gdf["UPRN"])
-    pipeline_uprns = set(pipeline_gdf["UPRN"])
-
-    n_council_uprns = council_tax_gdf["UPRN"].nunique()
-    n_pipeline_uprns = pipeline_gdf["UPRN"].nunique()
-
-    results = {
-        # Number of unique records / UPRNs per dataset
-        "N unique records in council tax data": raw_council_tax_df["PROPREF"].nunique(),
-        "N unique UPRNs in council tax data": n_council_uprns,
-        "N unique UPRNs in pipeline output data": n_pipeline_uprns,
-        # Differences in council tax UPRNs versus pipeline
-        "N diff UPRNs in pipeline minus council tax": n_pipeline_uprns
-        - n_council_uprns,
-        "Proportion diff UPRNs in pipeline minus council tax": round(
-            (n_pipeline_uprns - n_council_uprns) / n_pipeline_uprns, 3
-        ),
-        "N domestic UPRNs missing from pipeline": len(
-            council_uprns.difference(pipeline_uprns)
-        ),
-        "Proportion domestic UPRNs missing from pipeline": round(
-            len(council_uprns.difference(pipeline_uprns)) / n_council_uprns, 3
-        ),
-        "N UPRNs in pipeline but not council tax": len(
-            pipeline_uprns.difference(council_uprns)
-        ),
-        "Proportion UPRNs in pipeline not in council tax": round(
-            len(pipeline_uprns.difference(council_uprns)) / n_pipeline_uprns, 3
-        ),
-    }
-
-    return results
-
-
-def calculate_dict_building_diffs_per_dataset(
-    n_council_records: int,
-    council_tax_gdf: gpd.GeoDataFrame,
-    pipeline_gdf: gpd.GeoDataFrame,
-    buildings_gdf: gpd.GeoDataFrame,
-) -> dict:
-    """
-    Create a dictionary with a summary of the differences between building footprints containing domestic UPRNs identified
-    by the pipeline versus the council tax evaluation dataset.
-
-    Args:
-        n_council_records (int): total number of all records (including those without valid UPRNs) in the council tax data.
-        council_tax_gdf (gpd.GeoDataFrame): processed council tax data with empty UPRN rows removed and with geopoints per UPRN.
-        pipeline_gdf (gpd.GeoDataFrame): domestic UPRNs identified by current pipeline.
-        buildings_gdf (gpd.GeoDataFrame): building footprints for area of evaluation
-
-    Returns:
-        dict: summary of building differences between pipeline and evaluation datasets
-    """
-    # Get buildings containing council tax records
-    council_buildings_gdf = buildings_gdf.sjoin(
-        council_tax_gdf, how="inner", predicate="contains"
-    ).drop(columns="index_right")
-
-    # Get buildings containing domestic UPRNs identified by pipeline
-    pipeline_buildings_gdf = buildings_gdf.sjoin(
-        pipeline_gdf, how="inner", predicate="contains"
-    ).drop(columns="index_right")
-
-    # Get set of building IDs for each dataset
-    council_buildings = set(council_buildings_gdf["ID"])
-    pipeline_buildings = set(pipeline_buildings_gdf["ID"])
-
-    # Get the unique number of building IDs for each dataset
-    n_council_buildings = council_buildings_gdf["ID"].nunique()
-    n_pipeline_buildings = pipeline_buildings_gdf["ID"].nunique()
-
-    results = {
-        "Proportion of council records located in building footprints": round(
-            council_buildings_gdf["UPRN"].nunique() / n_council_records, 3
-        ),
-        "Proportion of council UPRNs located in building footprints": round(
-            council_buildings_gdf["UPRN"].nunique() / len(council_tax_gdf), 3
-        ),
-        "Proportion of pipeline UPRNs located in building footprints": round(
-            pipeline_buildings_gdf["UPRN"].nunique() / len(pipeline_gdf), 3
-        ),
-        "Proportion of council buildings in pipeline buildings": round(
-            len(council_buildings.intersection(pipeline_buildings))
-            / n_council_buildings,
-            3,
-        ),
-        "N pipeline buildings containing a council tax UPRN": len(
-            pipeline_buildings.intersection(council_buildings)
-        ),
-        "Proportion of pipeline buildings containing a council tax UPRN": round(
-            len(pipeline_buildings.intersection(council_buildings))
-            / n_pipeline_buildings,
-            3,
-        ),
-    }
-
-    return results
-
-
-def generate_gdf_false_positive_buildings(
-    council_tax_gdf: gpd.GeoDataFrame,
-    pipeline_gdf: gpd.GeoDataFrame,
-    buildings_gdf: gpd.GeoDataFrame,
-) -> gpd.GeoDataFrame:
-    """
-    Generate GeoDataFrame of true non-domestic buildings labelled as containing domestic UPRNs by the pipeline. Contains
-    predicted domestic UPRN count per building.
-
-    Args:
-        council_tax_gdf (gpd.GeoDataFrame): processed council tax data with empty UPRN rows removed and with geopoints per UPRN.
-        pipeline_gdf (gpd.GeoDataFrame): domestic UPRNs identified by current pipeline.
-        buildings_gdf (gpd.GeoDataFrame): building footprints for area of evaluation.
-
-    Returns:
-        gpd.GeoDataFrame: false positive buildings with predicted UPRN count
-    """
-    # Get buildings with council tax records
-    council_buildings_gdf = buildings_gdf.sjoin(
-        council_tax_gdf, how="inner", predicate="contains"
-    ).drop(columns="index_right")
-
-    # Get buildings with domestic UPRNs identified by pipeline
-    pipeline_buildings_gdf = buildings_gdf.sjoin(
-        pipeline_gdf, how="inner", predicate="contains"
-    ).drop(columns="index_right")
-
-    # Get council building IDs
-    council_buildings = set(council_buildings_gdf["ID"])
-
-    # Get false positive buildings from pipeline (pred: domestic, true: non-domestic)
-    false_positives_gdf = pipeline_buildings_gdf[
-        ~pipeline_buildings_gdf["ID"].isin(council_buildings)
-    ]
-
-    # Get pipeline UPRN count per false positive
-    uprns_per_building_gdf = false_positives_gdf.groupby("ID").agg(
-        UPRN_count=("UPRN", "nunique"), geometry=("geometry", "first")
-    )
-
-    return gpd.GeoDataFrame(
-        uprns_per_building_gdf, geometry="geometry", crs="EPSG:27700"
-    )
-
-
 def label_gdf_buildings_domestic_bool(
     buildings_gdf: gpd.GeoDataFrame,
     uprn_gdf: gpd.GeoDataFrame,
@@ -554,6 +393,7 @@ if __name__ == "__main__":
     raw_council_tax_uprns_df = pd.read_csv(
         config["data"]["geodata"]["council_tax_data"]["plymouth"]
     )
+    council_tax_uprns_gdf = transform_gdf_council_tax(raw_council_tax_uprns_df)
 
     # Pipeline outputs
     pipeline_domestic_uprns_df = base_getters.load_df_from_s3(
@@ -576,35 +416,6 @@ if __name__ == "__main__":
     # All UPRNs with geometries
     uprns_df = load_geodata.load_df_osopen_uprn()
     uprns_gdf = uprns.generate_gdf_uprn_coords(uprns_df)
-
-    # ------------------------------------------------------------------- #
-    # ANALYSIS - COMPARISON OF CURRENT PIPELINE WITH COUNCIL TAX DATA
-    # ------------------------------------------------------------------- #
-    n_council_records = len(raw_council_tax_uprns_df)
-    council_tax_uprns_gdf = transform_gdf_council_tax(raw_council_tax_uprns_df)
-
-    results = calculate_dict_uprn_diffs_per_dataset(
-        raw_council_tax_uprns_df, council_tax_uprns_gdf, pipeline_domestic_uprns_gdf
-    )
-    buildings_results = calculate_dict_building_diffs_per_dataset(
-        n_council_records=n_council_records,
-        council_tax_gdf=council_tax_uprns_gdf,
-        pipeline_gdf=pipeline_domestic_uprns_gdf,
-        buildings_gdf=buildings_gdf,
-    )
-
-    false_positives_gdf = generate_gdf_false_positive_buildings(
-        council_tax_gdf=council_tax_uprns_gdf,
-        pipeline_gdf=pipeline_domestic_uprns_gdf,
-        buildings_gdf=buildings_gdf,
-    )
-    mapping_utils.plot_folium_polygon_map(
-        boundary=plymouth_boundary,
-        gdf_dict={"False positives": false_positives_gdf},
-        colour_mapping={"False positives": "red"},
-        popup_col="UPRN_count",
-        save_as="plymouth_false_positive_domestic_buildings",
-    )
 
     # ------------------------------------------------------ #
     # FIND FEATURE AND THRESHOLD TO IMPROVE FILTERING
