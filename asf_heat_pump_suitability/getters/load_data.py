ec1dd431473fd1f3f92c771318bf5583b672e805
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-05-19 18:46:08 +0100

    delete getters for postcode dir and old INSPIRE gette from load_data

--- asf_heat_pump_suitability/getters/load_data.py
+++ asf_heat_pump_suitability/getters/load_data.py
@@ -15,26 +15,6 @@ from typing import Tuple
 warnings.filterwarnings("ignore", category=RuntimeWarning, message="VSIFSeekL")
 
 
-def get_df_ons_pd(**kwargs) -> pl.DataFrame:
-    """
-    Get ONS postcode directory (ONSPD) for Great Britain.
-
-    Args:
-        **kwargs for pl.read_csv
-
-    Returns:
-        pl.DataFrame: postcode directory for Great Britain
-    """
-    df = base_getters.get_df_from_zip_url(
-        url=config["data_source"]["gb_ons_postcode_dir_url"],
-        extract_file=config["data_source"]["gb_ons_postcode_dir_file_path"],
-        schema=schemas.onspd_schema,
-        **kwargs,
-    )
-
-    return df
-
-
 def load_gdf_ons_council_bounds(**kwargs) -> gpd.GeoDataFrame:
     """
     Load ONS council bounding polygons for the UK (CRS: EPSG:27700).
@@ -80,25 +60,6 @@ def load_gdf_microsoft_building_footprints(url: str) -> gpd.GeoDataFrame:
     gdf = gpd.read_file(
         f"GeoJSONSeq:/vsigzip//vsicurl/{url}", engine="pyogrio", use_arrow=True
     )
-
-    return gdf
-
-
-@retry(stop=stop_after_attempt(4))
-def load_gdf_inspire_land_parcels(path: str, **kwargs) -> gpd.GeoDataFrame:
-    """
-    Load land registry's index polygons spatial data (INSPIRE) showing the geometry and extent of registered freehold
-    properties in England and Wales. CRS EPSG:27700, British National Grid.
-
-    Args:
-        path (str): path to INSPIRE land parcel file
-        **kwargs for `gpd.read_file()`
-
-    Returns:
-        gpd.GeoDataFrame: registered land extent polygons for one council
-    """
-    logging.info(f"Loading INSPIRE land parcel file: {path}")
-    gdf = gpd.read_file(path, engine="pyogrio", **kwargs)
 
     return gdf
 
