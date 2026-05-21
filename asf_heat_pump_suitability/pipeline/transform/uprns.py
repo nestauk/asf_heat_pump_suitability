7bafa248577f1f4c0a23fe2abb0ffca9ffa65293
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-05-21 17:23:56 +0100

    remove unused parameter in UPRN to building mapping function in uprns.py

--- asf_heat_pump_suitability/pipeline/transform/uprns.py
+++ asf_heat_pump_suitability/pipeline/transform/uprns.py
@@ -270,7 +270,6 @@ def map_dict_uprns_to_building_id(
     uprns_gdf: gpd.GeoDataFrame,
     buildings_gdf: gpd.GeoDataFrame,
     id_col: str,
-    predicate: str = "intersects",
     max_distance: float = 1,
 ) -> dict:
     """
@@ -280,8 +279,6 @@ def map_dict_uprns_to_building_id(
         uprns_gdf (gpd.GeoDataFrame): UPRNs with geospatial point data
         buildings_gdf (gpd.GeoDataFrame): building footprints
         id_col (str): name of building ID column in `buildings_gdf`
-        predicate (str): how to join buildings and UPRNs. Can be one of: `intersects`, which joins UPRNs with building footprints
-        they intersect with, or `within` which joins UPRNs to building footprints they are located within. Default `intersects`.
         max_distance (float): max distance (metres) from which to join UPRNs to a building footprint. Default 1m.
 
     Returns:
