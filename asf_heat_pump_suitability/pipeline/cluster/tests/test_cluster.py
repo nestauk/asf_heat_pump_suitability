fd5ed298d8f7ec3d784e77bd04b20799853a0e3d
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-06-10 17:17:06 +0100

    add first clustering tests to test_cluster.py

--- asf_heat_pump_suitability/pipeline/cluster/tests/test_cluster.py
+++ asf_heat_pump_suitability/pipeline/cluster/tests/test_cluster.py
@@ -1,3 +1,5 @@
+import pytest
+import geopandas as gpd
 from asf_heat_pump_suitability.pipeline.cluster.cluster import (
     extend_edges_gdf,
     generate_gdf_clusters,
@@ -5,17 +7,166 @@ from asf_heat_pump_suitability.pipeline.cluster.cluster import (
 
 
 class TestGenerateGdfClusters:
-    def test_clusters_not_overlapping(self):
-        pass
+    @pytest.fixture(scope="class")
+    def buildings_gdf(self):
+        return gpd.GeoDataFrame()
 
-    def test_clusters_entirely_contain_buildings(self):
-        pass
+    @pytest.fixture(scope="class")
+    def boundary_gdf(self):
+        return gpd.GeoDataFrame()
 
-    def test_no_empty_clusters(self):
-        pass
+    @pytest.fixture(scope="class")
+    def tech_gdf(self):
+        return gpd.GeoDataFrame()
 
-    def test_clusters_contain_domestic_only(self):
-        pass
+    @pytest.fixture(scope="class")
+    def polygon_overlay_gdf(self):
+        return gpd.GeoDataFrame()
+
+    @pytest.fixture(scope="class")
+    def line_overlay_gdf(self):
+        return gpd.GeoDataFrame()
+
+    @pytest.fixture(scope="class")
+    def anchor_gdf(self):
+        return gpd.GeoDataFrame()
+
+    def test_all_domestic_buildings_assigned_cluster(
+        self,
+        buildings_gdf,
+        boundary_gdf,
+        tech_gdf,
+        line_overlay_gdf,
+        polygon_overlay_gdf,
+        anchor_gdf,
+    ):
+        """Test each domestic building is assigned to a cluster."""
+        clusters_gdf = generate_gdf_clusters(
+            buildings_gdf=buildings_gdf,
+            boundary_gdf=boundary_gdf,
+            tech_gdf=tech_gdf,
+            line_overlay_gdf=line_overlay_gdf,
+            polygon_overlay_gdf=polygon_overlay_gdf,
+            combined_anchor_gdf=anchor_gdf,
+            radius=50,
+            id_col="building_id",
+        )
+        results = clusters_gdf.sjoin(buildings_gdf, how="inner", predicate="contains")[
+            "building_id"
+        ]
+        expected = buildings_gdf["building_id"]
+
+        assert set(results) == set(expected)
+
+    def test_clusters_not_overlapping(
+        self,
+        buildings_gdf,
+        boundary_gdf,
+        tech_gdf,
+        line_overlay_gdf,
+        polygon_overlay_gdf,
+        anchor_gdf,
+    ):
+        """Test there are no overlapping clusters."""
+        results = generate_gdf_clusters(
+            buildings_gdf=buildings_gdf,
+            boundary_gdf=boundary_gdf,
+            tech_gdf=tech_gdf,
+            line_overlay_gdf=line_overlay_gdf,
+            polygon_overlay_gdf=polygon_overlay_gdf,
+            combined_anchor_gdf=anchor_gdf,
+            radius=50,
+            id_col="building_id",
+        )
+
+        assert round(results["geometry"].area.sum(), 8) == round(
+            results["geometry"].union_all().area, 8
+        )
+
+    def test_clusters_entirely_contain_buildings(
+        self,
+        buildings_gdf,
+        boundary_gdf,
+        tech_gdf,
+        line_overlay_gdf,
+        polygon_overlay_gdf,
+        anchor_gdf,
+    ):
+        """Test clusters entirely contain buildings."""
+        clusters_gdf = generate_gdf_clusters(
+            buildings_gdf=buildings_gdf,
+            boundary_gdf=boundary_gdf,
+            tech_gdf=tech_gdf,
+            line_overlay_gdf=line_overlay_gdf,
+            polygon_overlay_gdf=polygon_overlay_gdf,
+            combined_anchor_gdf=anchor_gdf,
+            radius=50,
+            id_col="building_id",
+        )
+
+        results = clusters_gdf.sjoin(buildings_gdf, how="inner", predicate="contains")[
+            "building_id"
+        ]
+        expected = buildings_gdf["building_id"]
+
+        assert set(results) == set(expected)
+
+    def test_no_empty_clusters(
+        self,
+        buildings_gdf,
+        boundary_gdf,
+        tech_gdf,
+        line_overlay_gdf,
+        polygon_overlay_gdf,
+        anchor_gdf,
+    ):
+        """Test there are no clusters that do not match to a building."""
+        clusters_gdf = generate_gdf_clusters(
+            buildings_gdf=buildings_gdf,
+            boundary_gdf=boundary_gdf,
+            tech_gdf=tech_gdf,
+            line_overlay_gdf=line_overlay_gdf,
+            polygon_overlay_gdf=polygon_overlay_gdf,
+            combined_anchor_gdf=anchor_gdf,
+            radius=50,
+            id_col="building_id",
+        )
+
+        results = (
+            clusters_gdf.sjoin(buildings_gdf, how="left", predicate="contains")[
+                "building_id"
+            ]
+            .isna()
+            .sum()
+        )
+        assert results == 0
+
+    def test_clusters_contain_domestic_only(
+        self,
+        buildings_gdf,
+        boundary_gdf,
+        tech_gdf,
+        line_overlay_gdf,
+        polygon_overlay_gdf,
+        anchor_gdf,
+    ):
+        """Test that there are only domestic building footprints in the clusters."""
+        clusters_gdf = generate_gdf_clusters(
+            buildings_gdf=buildings_gdf,
+            boundary_gdf=boundary_gdf,
+            tech_gdf=tech_gdf,
+            line_overlay_gdf=line_overlay_gdf,
+            polygon_overlay_gdf=polygon_overlay_gdf,
+            combined_anchor_gdf=anchor_gdf,
+            radius=50,
+            id_col="building_id",
+        )
+        results = clusters_gdf.sjoin(buildings_gdf, how="inner", predicate="contains")[
+            "building_id"
+        ]
+        expected = buildings_gdf[buildings_gdf["type"] == "domestic"]
+
+        assert set(results) == set(expected)
 
     def test_neighbouring_clusters_different(self):
         """Check dissolve worked properly"""
