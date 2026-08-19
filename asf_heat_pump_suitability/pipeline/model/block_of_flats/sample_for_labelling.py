aa081ed2b011ebd5ed703e5c0cf1ff9985753205
roisin <104171770+crispy-wonton@users.noreply.github.com>
2026-08-19 15:53:18 +0100

    map building ID to UPRN in sample for labelling script

--- asf_heat_pump_suitability/pipeline/model/block_of_flats/sample_for_labelling.py
+++ asf_heat_pump_suitability/pipeline/model/block_of_flats/sample_for_labelling.py
@@ -4,8 +4,9 @@ Create a sample of buildings containing flats for manual labelling to use in mod
 
 if "name" == "__main__":
     import polars as pl
-    from asf_heat_pump_suitability.getters import load_data
+    from asf_heat_pump_suitability.getters import load_data, load_geodata
     from asf_heat_pump_suitability.pipeline.impute import property_type
+    from asf_heat_pump_suitability.pipeline.transform import uprns
 
     # Load our domestic UPRNs from processing
     domestic_uprns = set(pl.read_parquet("path", columns="UPRN")["UPRN"])
@@ -19,6 +20,19 @@ if "name" == "__main__":
     )
     uprns_df = uprns_df.with_columns(pl.col("UPRN").is_in(flat_uprns).alias("is_flat"))
 
+    # Load building footprints and map to UPRNs
+    buildings_gdf = load_geodata.load_gdf_os_openmap_layer(layer="building")
+    uprns_gdf = uprns.generate_gdf_uprn_coords(uprns_df, usecols=["UPRN", "is_flat"])
+    uprn_building_mapping = uprns.map_dict_uprns_to_building_id(
+        uprns_gdf=uprns_gdf, buildings_gdf=buildings_gdf, id_col="ID"
+    )
+    uprns_df = uprns_df.with_columns(
+        # Map building IDs to the UPRNs they contain
+        pl.col("UPRN")
+        .replace_strict(uprn_building_mapping, default=None)
+        .alias("building_id")
+    )
+
     flats_df = uprns_df.filter(pl.col("is_flat"))
 
 
@@ -27,7 +41,7 @@ if "name" == "__main__":
 # 2. [DONE] Identify all buildings containing flats.
 # 3. [DONE] Label buildings with rural/urban indicator
 # 4. [DONE] Label buildings with IMD decile
-# 5. Label buildings with country
+# 5. [DONE] Label buildings with country
 # 7. Get count of flats per building
 # 8. Label buildings with grouped-count
 # 9. Sample based on rural/urban indicator; IMD decile; country; and grouped count
